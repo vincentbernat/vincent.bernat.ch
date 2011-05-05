b2084d6b797fcc729c1ccbe374fca29ef8a02b08
Vincent Bernat <bernat@luffy.cx>
2011-05-05 22:56:38 +0200

    Add multilanguage stuff.

--- content/media/js/luffy.js
+++ content/media/js/luffy.js
@@ -3,4 +3,8 @@ $(function() {
     /* Set appropriate height for rotated pages */
     $("#lf-page-1,#lf-page-2")
 	.css("height",$("#lf-page").innerHeight());
+    /* Animate the language box */
+    $("#lf-translations").css('left',
+			      '-' + $("#lf-translations").css('width'))
+	.animate({left: 0}, 'slow');
 })
