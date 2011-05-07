452fac42935b4332ad42e6ee39bb31d19a78fdb7
Vincent Bernat <bernat@luffy.cx>
2011-05-07 21:58:28 +0200

    Enhance search engine.
    But don't use Google Custom Search Engine: this seems pretty invasive.

--- content/media/js/luffy.search.js
+++ content/media/js/luffy.search.js
@@ -1,6 +1,23 @@
-/* Search engine using Google Custom Search */
+/* Search engine (not using Google Custome Search Engine) */
 
 var luffy = luffy || {};
 luffy.search = function() {
-    
+    /* Clicking the search glass submit the form */
+    $("#lf-search-glass").click(function() {
+	$("#lf-search").submit();
+    });
+    /* Clicking in the search box change the background to white */
+    $("#lf-search-query").focus(function() {
+	$(this)
+	    .css("background-color", "white")
+	    .css("color", "black");
+
+    });
+    $("#lf-search-query").blur(function() {
+	if ($(this).val() == "") {
+	    $(this)
+		.css("background-color", "#666")
+		.css("color", "#ccc");
+	}
+    });
 };
