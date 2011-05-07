81f2ef860c10188c77fc6dfd1f026da34ce83210
Vincent Bernat <bernat@luffy.cx>
2011-05-07 02:41:47 +0200

    Avoid flickering by hiding background pages in CSS.

--- content/media/js/luffy.effects.js
+++ content/media/js/luffy.effects.js
@@ -5,7 +5,7 @@ luffy.effects = function() {
     $(function() {
 	/* Set appropriate height for rotated pages */
 	$("#lf-page-1,#lf-page-2")
-	    .css("height",$("#lf-page").innerHeight());
+	    .css("height",$("#lf-page").innerHeight()).show();
 	/* Animate the language box */
 	$("#lf-translations").css('left',
 				  '-' + $("#lf-translations").css('width'))
