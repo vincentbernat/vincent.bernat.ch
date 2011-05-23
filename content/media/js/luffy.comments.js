68c914c3bb3da38a76588add900201ba16549cde
Vincent Bernat <bernat@luffy.cx>
2011-05-23 20:51:05 +0200

    Harmonizing updates through articles

--- content/media/js/luffy.comments.js
+++ content/media/js/luffy.comments.js
@@ -16,13 +16,13 @@ luffy.comments = function() {
 	var comment = location.hash.substr(9);
 	var style = "#dsq-comment-" + comment + " .dsq-comment-header { " +
 	    "background-color: #FBE686; background-image: none !important; border:3px solid #FBC586; }";
+	/* Highlight the target comment */
+	$("head").append("<style>" + style + "</style>");
 	/* Scroll to comments before Disqus is loading */
 	if (typeof window.scroll == 'function') {
 	    window.scroll(0, $("#lf-disqus").offset().top);
 	}
 	/* Make Disqus load */
 	$("#lf-disqus").click();
-	/* Highlight the target comment */
-	$("head").append("<style>" + style + "</style>");
     }
 };
