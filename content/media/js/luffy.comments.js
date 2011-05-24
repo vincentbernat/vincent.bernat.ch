3404a082897933f44e34c0f1c1984d780d0164d2
Vincent Bernat <bernat@luffy.cx>
2011-05-24 07:31:11 +0200

    GA: add an event to track whetever comments are used or not.

--- content/media/js/luffy.comments.js
+++ content/media/js/luffy.comments.js
@@ -5,6 +5,7 @@ luffy.comments = function() {
     $("#lf-disqus").click(function() {
 	var loading = $(this).text('Loading/Chargement...');
         var src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+	_gaq.push(['_trackEvent', 'Comments', 'Display']);
 	yepnope({ load: src,
 		  complete: function() {
 		      loading.hide();
