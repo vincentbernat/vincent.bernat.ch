1dbcf88eac1d074cb367a98cf9623c966a753a76
Vincent Bernat <bernat@luffy.cx>
2011-05-26 21:34:25 +0200

    JS: don't animate sidebar. CPU waste.

--- content/media/js/luffy.effects.js
+++ content/media/js/luffy.effects.js
@@ -5,8 +5,4 @@ luffy.effects = function() {
     /* Set appropriate height for rotated pages */
     $(".csstransforms #lf-page-1, .csstransforms #lf-page-2")
 	.css("height",$("#lf-page").innerHeight()).show();
-    /* Animate the language box */
-    $("#lf-links").css('left',
-		       '-' + $("#lf-links").css('width'))
-	.animate({left: 0}, 'slow');
 };
