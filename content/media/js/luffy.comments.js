28215c7a6089cac2f680da50263da96c25051eea
Vincent Bernat <bernat@luffy.cx>
2011-06-13 09:31:58 +0200

    JS: when clicking on a comment link for the same page, display the comments

--- content/media/js/luffy.comments.js
+++ content/media/js/luffy.comments.js
@@ -2,6 +2,7 @@
 
 var luffy = luffy || {};
 luffy.comments = function() {
+    /* Load Disqus when clicking on the comment bar */
     $("#lf-disqus").click(function() {
 	var loading = $(this).text('Loading/Chargement...');
         var src = '//' + disqus_shortname + '.disqus.com/embed.js';
@@ -15,9 +16,8 @@ luffy.comments = function() {
 		  }});
     });
 
-    /* Display comments now if there is a comment anchor in our URL */
-    if (location.hash.match("^#comment-")) {
-	var comment = location.hash.substr(9);
+    /* Display comments when the current location point to a comment */
+    var display_comment = function(comment) {
 	var style = "#dsq-comment-" + comment + " .dsq-comment-header { " +
 	    "background-color: #FBE686; background-image: none !important; border:3px solid #FBC586; }";
 	/* Highlight the target comment */
@@ -29,4 +29,21 @@ luffy.comments = function() {
 	/* Make Disqus load */
 	$("#lf-disqus").click();
     }
+    /* Display comments now if there is a comment anchor in our URL */
+    if (location.hash.match("^#comment-")) {
+	var comment = location.hash.substr(9);
+	display_comment(comment);
+    }
+    /* Also display them when we click on a link to a comment to the
+       same page */
+    $("article a").filter(function (index) {
+	return (this.host == location.host) &&
+	    (this.pathname == location.pathname ||
+	     "/" + this.pathname == location.pathname) &&
+	    (typeof this.hash !== 'undefined') &&
+	    (this.hash.match("^#comment-"));
+    }).click(function() {
+	var comment = this.hash.substr(9);
+	display_comment(comment);
+    });
 };
