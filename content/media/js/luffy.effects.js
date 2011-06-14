b3ec1a3e262514244c97140787c350b220118820
Vincent Bernat <bernat@luffy.cx>
2011-06-14 20:06:20 +0200

    JS: resize pages on window size change

--- content/media/js/luffy.effects.js
+++ content/media/js/luffy.effects.js
@@ -2,9 +2,23 @@
 
 var luffy = luffy || {};
 luffy.effects = function() {
+    var resizeTimer = null;
+
     /* Set appropriate height for rotated pages */
-    $(".csstransforms #lf-page-1, .csstransforms #lf-page-2")
-	.css("height",$("#lf-page").innerHeight())
-	.css("width", $("#lf-page").innerWidth())
-	.show();
+    var updatePageSizes = function() {
+	$(".csstransforms #lf-page-1, .csstransforms #lf-page-2")
+	    .css("height",$("#lf-page").innerHeight())
+	    .css("width", $("#lf-page").innerWidth())
+	    .show();
+	resizeTimer = null;
+    }
+    
+    /* Fire on start */
+    updatePageSizes();
+
+    /* Fire on resize (but not too often) */
+    $(window).resize(function() {
+	if (resizeTimer) clearTimeout(resizeTimer);
+	resizeTimer = setTimeout(updatePageSizes, 100);
+    });
 };
