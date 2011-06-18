cbfd39eb2b3cf8420f5bdd52bd8384e4e40bd877
Vincent Bernat <bernat@luffy.cx>
2011-06-18 18:31:25 +0200

    JS: add scrolly headers

--- content/media/js/luffy.effects.js
+++ content/media/js/luffy.effects.js
@@ -40,4 +40,34 @@ luffy.effects = function() {
 		       win.width()]);
 	}
     }();
+
+    /* -- Effect 3:
+          Scrolly headers (stolen from Steve Losh)
+	  https://github.com/sjl/stevelosh/blob/master/media/js/sjl.js
+       -- */
+    e = function() {
+	var header = $('<div id="lf-scrolling-header" />');
+	var h1s = $("#lf-main h1");
+	var soff = 75;
+	$('body').append(header[0]);
+	$(window).scroll(function() {
+	    var width = $("#lf-pages").first().position()['left'] - 25;
+	    if (width < 50) return;
+	    var y = $(window).scrollTop(); // Current position
+	    var title = null;		   // Title to display
+	    h1s.each(function() {
+		// Do we need to display this specific header?
+		var header_y = $(this).position()['top'] - soff;
+		if (y < header_y) return false;
+		// typogrify is adding a lot of non breakable spaces
+		title = $(this).html().replace(/&nbsp;/g, ' ');
+	    });
+	    if (title === null) {
+		header.hide();
+	    } else {
+		header.css({ top: soff,
+			     width: width }).html(title).show();
+	    }
+	});
+    }();
 };
