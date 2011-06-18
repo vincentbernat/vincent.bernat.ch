797740aff777f94da51b64bd137b004288ef734f
Vincent Bernat <bernat@luffy.cx>
2011-06-18 17:49:53 +0200

    JS: properly document effects and place them in their own scope

--- content/media/js/luffy.effects.js
+++ content/media/js/luffy.effects.js
@@ -2,32 +2,42 @@
 
 var luffy = luffy || {};
 luffy.effects = function() {
-    var resizeTimer = null;
+    var e;
+    /* -- Effect 1:
+          Resize #lf-page-{1,2} to match size of main page
+       -- */
+    e = function() {
+	var resizeTimer = null;
 
-    /* Set appropriate height for rotated pages */
-    var updatePageSizes = function() {
-	$(".csstransforms #lf-page-1, .csstransforms #lf-page-2")
-	    .css("height",$("#lf-page").innerHeight())
-	    .css("width", $("#lf-page").innerWidth())
-	    .show();
-	resizeTimer = null;
-    }
-    
-    /* Fire on start */
-    updatePageSizes();
+	/* Set appropriate height for rotated pages */
+	var updatePageSizes = function() {
+	    $(".csstransforms #lf-page-1, .csstransforms #lf-page-2")
+		.css("height",$("#lf-page").innerHeight())
+		.css("width", $("#lf-page").innerWidth())
+		.show();
+	    resizeTimer = null;
+	}
 
-    /* Fire on resize (but not too often) */
-    $(window).resize(function() {
-	if (resizeTimer) clearTimeout(resizeTimer);
-	resizeTimer = setTimeout(updatePageSizes, 100);
-    });
+	/* Fire on start */
+	updatePageSizes();
 
-    /* What's window size anyway? */
-    if (typeof(_gaq) !== 'undefined') {
-	var win = $(window);
-	_gaq.push(['_trackEvent', 'Viewport',
-		   'Size',
-		   win.width() + 'x' + win.height(),
-		   win.width()]);
-    }
+	/* Fire on resize (but not too often) */
+	$(window).resize(function() {
+	    if (resizeTimer) clearTimeout(resizeTimer);
+	    resizeTimer = setTimeout(updatePageSizes, 100);
+	});
+    }();
+
+    /* -- Effect 2:
+          Log the window size to Google Analytics (not really an effect)
+       -- */
+    e = function() {
+	if (typeof(_gaq) !== 'undefined') {
+	    var win = $(window);
+	    _gaq.push(['_trackEvent', 'Viewport',
+		       'Size',
+		       win.width() + 'x' + win.height(),
+		       win.width()]);
+	}
+    }();
 };
