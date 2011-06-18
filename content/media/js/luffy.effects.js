1770e941c8201fc66d403c59e42b105e76237c12
Vincent Bernat <bernat@luffy.cx>
2011-06-18 19:00:51 +0200

    CSS: remove multicolumns for wide screen
    This was incompatible with scrolly header. Nobody has a screen so
    large (or they don't use their browser full screen).

--- content/media/js/luffy.effects.js
+++ content/media/js/luffy.effects.js
@@ -7,25 +7,13 @@ luffy.effects = function() {
           Resize #lf-page-{1,2} to match size of main page
        -- */
     e = function() {
-	var resizeTimer = null;
-
 	/* Set appropriate height for rotated pages */
 	var updatePageSizes = function() {
 	    $(".csstransforms #lf-page-1, .csstransforms #lf-page-2")
 		.css("height",$("#lf-page").innerHeight())
 		.css("width", $("#lf-page").innerWidth())
 		.show();
-	    resizeTimer = null;
-	}
-
-	/* Fire on start */
-	updatePageSizes();
-
-	/* Fire on resize (but not too often) */
-	$(window).resize(function() {
-	    if (resizeTimer) clearTimeout(resizeTimer);
-	    resizeTimer = setTimeout(updatePageSizes, 100);
-	});
+	}();
     }();
 
     /* -- Effect 2:
