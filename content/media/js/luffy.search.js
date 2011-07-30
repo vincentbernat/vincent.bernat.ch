eac4077d8c3204f399f3c07d4b962204b0924f44
Vincent Bernat <bernat@luffy.cx>
2011-07-30 12:52:42 +0200

    css: rework a bit how the search engine is displayed

--- content/media/js/luffy.search.js
+++ content/media/js/luffy.search.js
@@ -1,23 +1,19 @@
-/* Search engine (not using Google Custome Search Engine) */
+/* Search engine */
 
 var luffy = luffy || {};
 luffy.search = function() {
-    /* Clicking the search glass submit the form */
-    $("#lf-search-glass").click(function() {
-	$("#lf-search").submit();
-    });
     /* Clicking in the search box change the background to white */
     $("#lf-search-query").focus(function() {
 	$(this)
-	    .css("background-color", "white")
-	    .css("color", "black");
+	    .css({"background-color": "white",
+		  color: "black"});
 
     });
     $("#lf-search-query").blur(function() {
 	if ($(this).val() == "") {
 	    $(this)
-		.css("background-color", "#666")
-		.css("color", "#ccc");
+		.css({"background-color": "#666",
+		      color: "#ccc"});
 	}
     });
 };
