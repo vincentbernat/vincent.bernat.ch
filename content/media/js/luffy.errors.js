3012d1bac7e97018d9e2f4d9997ce72b66aec0d7
Vincent Bernat <bernat@luffy.cx>
2011-09-10 11:26:44 +0200

    js: Drop error handling to GA (difficult to use)

--- content/media/js/luffy.errors.js
+++
@@ -1,8 +0,0 @@
-/* On errors, forward to Google Analytics */
-if (typeof(_gaq) !== 'undefined') {
-    window.onerror = function(message, file, line) {
-	_gaq.push(['_trackEvent', 'Errors',
-		   file + ':' + line,
-		   message + '']);
-    }
-}
