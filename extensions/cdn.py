9972dbb097be488a4a7ab924da4b60a5d212b6af
Vincent Bernat <bernat@luffy.cx>
2011-11-23 20:26:53 +0100

    Don't serve everything from CDN: exclude files/ and videos/ (too big)

---
+++ extensions/cdn.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Plugin to use a CDN for some static files
+"""
+
+import time
+
+import os
+from functools import wraps
+from urllib import quote
+
+from hyde.plugin import Plugin
+from hyde.site import Site
+from hyde.fs import FS, File, Folder
+
+# Serve everything from CDN except content of files/ and videos/
+NOCDN="//media.luffy.cx/"
+def decorate_media_url(media_url):
+    @wraps(media_url)
+    def wrapper(site, path):
+        if path.startswith("files/") or path.startswith("videos/"):
+            # Don't use CDN for those big files
+            return quote(Folder(NOCDN).child(path).replace(os.sep, '/').encode("utf-8"))
+        return media_url(site, path)
+    return wrapper
+
+
+class CdnPlugin(Plugin):
+    def __init__(self, site):
+        super(CdnPlugin, self).__init__(site)
+
+    def begin_site(self):
+        """
+        Initialize the plugin.
+
+        We just decorate `Site.media_url` function.
+        """
+        Site.media_url = decorate_media_url(Site.media_url)
