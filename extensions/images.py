a41c2b908b8f04927596b16243970f0408e4baa5
Vincent Bernat <bernat@luffy.cx>
2011-12-22 19:27:30 +0100

    Move thumbnail extension from hyde to extensions/
    This allows to use a regular hyde installation instead of a patched
    one.

---
+++ extensions/images.py
@@ -0,0 +1,98 @@
+# -*- coding: utf-8 -*-
+"""
+Contains classes to handle images related things
+
+# Requires PIL
+"""
+
+from hyde.plugin import Plugin
+from hyde.fs import File, Folder
+
+import Image
+import new
+import os
+from functools import partial
+
+class Thumb(object):
+    def __init__(self, path, **kwargs):
+        self.path = path
+        for arg in kwargs:
+            setattr(self, arg, kwargs[arg])
+    def __str__(self):
+        return self.path       
+
+def thumb(self, defaults={}, width=None, height=None):
+    """
+    Generate a thumbnail for the given image
+    """
+    if width is None and height is None:
+        width, height = defaults['width'], defaults['height']
+    im = Image.open(self.path)
+    if im.mode != 'RGBA':
+        im = im.convert('RGBA')
+    # Convert to a thumbnail
+    if width is None:
+        # height is not None
+        width = im.size[0]*height/im.size[1] + 1
+    elif height is None:
+        # width is not None
+        height = im.size[1]*width/im.size[0] + 1
+    im.thumbnail((width, height), Image.ANTIALIAS)
+    # Prepare path
+    path = os.path.join(os.path.dirname(self.get_relative_deploy_path()),
+                        "%s%s" % (defaults['prefix'],
+                                  self.name))
+    target = File(Folder(self.site.config.deploy_root_path).child(path))
+    target.parent.make()
+    if self.name.endswith(".jpg"):
+        im.save(target.path, "JPEG", optimize=True, quality=75)
+    else:
+        im.save(target.path, "PNG", optimize=True)
+    return Thumb(path, width=im.size[0], height=im.size[1])
+
+class ImageThumbnailsPlugin(Plugin):
+    """
+    Provide a function to get thumbnail for any image resource.
+
+    Each image resource will get a `thumb()` function. This function
+    can take the following keywords:
+      - width (int)
+      - height (int)
+      - prefix (string): prefix to use for thumbnails
+
+    This plugin can be configured with the exact same keywords to set
+    site defaults. The `thumb()` function will return a path to the
+    thumbnail. This path will have `width` and `height` as an
+    attribute.
+
+    Thumbnails are created in the same directory of their image.
+
+    Currently, only supports PNG and JPG.
+    """
+
+    def __init__(self, site):
+        super(ImageThumbnailsPlugin, self).__init__(site)
+
+    def begin_site(self):
+        """
+        Find any image resource to add them the thumb() function.
+        """
+        # Grab default values from config
+        config = self.site.config
+        defaults = { "width": None,
+                     "height": 40,
+                     "prefix": 'thumb_',
+                     }
+        if hasattr(config, 'thumbnails'):
+            defaults.update(config.thumbnails)
+        # Make the thumbnailing function
+        thumbfn = partial(thumb, defaults=defaults)
+        thumbfn.__doc__ = "Create a thumbnail for this image"
+
+        # Add it to any image resource
+        for node in self.site.content.walk():
+            for resource in node.resources:
+                if resource.source_file.kind not in ["jpg", "png"]:
+                    continue
+                self.logger.debug("Adding thumbnail function to [%s]" % resource)
+                resource.thumb = new.instancemethod(thumbfn, resource, resource.__class__)
