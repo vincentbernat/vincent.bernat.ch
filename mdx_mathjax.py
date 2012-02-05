115c5a8eeafc0918843429b7d92a12ed72cbf607
Vincent Bernat <bernat@luffy.cx>
2012-02-05 12:03:53 +0100

    Add a MathJax markdown plugin.
    This plugin avoids escaping of '\' in LaTeX. Stolen from:
     https://github.com/mayoff/python-markdown-mathjax/blob/master/mdx_mathjax.py

---
+++ mdx_mathjax.py
@@ -0,0 +1,24 @@
+# -*- encoding: utf-8 -*-
+# Skip any processing for · ... · or ·· ... ··
+# This will be processed later by MathJax
+
+import markdown
+etree = markdown.etree_loader.importETree()
+
+class MathJaxPattern(markdown.inlinepatterns.Pattern):
+
+    def __init__(self):
+        markdown.inlinepatterns.Pattern.__init__(self, r'(?<!\\)(··?)(.+?)\2')
+
+    def handleMatch(self, m):
+        node = etree.Element("latex")
+        node.text = markdown.util.AtomicString(m.group(2) + m.group(3) + m.group(2))
+        return node
+
+class MathJaxExtension(markdown.Extension):
+    def extendMarkdown(self, md, md_globals):
+        # Needs to come before escape matching because \ is pretty important in LaTeX
+        md.inlinePatterns.add('mathjax', MathJaxPattern(), '<escape')
+
+def makeExtension(configs=None):
+    return MathJaxExtension(configs)
