3506131bf14549fc8e7493bf2b6460e94de96e5a
Vincent Bernat <bernat@luffy.cx>
2012-04-10 14:55:37 +0200

    js: log errors to Google Analytics

--- content/media/js/luffy.js
+++ content/media/js/luffy.js
@@ -4,6 +4,16 @@ combine:
     where: top
     remove: yes
 ---
+/* Log errors to Google Analytics.
+   See: http://www.thetaboard.com/blog/client-side-error-logging-with-google-analytics
+*/
+if (typeof(_gaq) !== 'undefined') {
+    window.onerror = function(message, file, line) {
+	var sFormattedMessage = '[' + file + ' (' + line + ')] ' + message;
+	_gaq.push(['_trackEvent', 'Exceptions', 'Application',
+		   sFormattedMessage, null, true]);
+    }
+}
 
 $(function() {
     luffy.mathjax();
