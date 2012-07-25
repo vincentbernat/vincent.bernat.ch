0cdfd131aa763fb2c09aaa13d225f6e5ca937ed6
Vincent Bernat <bernat@luffy.cx>
2012-07-25 18:06:37 +0200

    typography: rework widow plugin
    First, when a sentence do not end by a punctuation mark, don't try to
    do widow detection. We don't want that for titles or incomplete
    sentences (they are usually short).
    
    Second, in French, a double punctuation mark is preceded by a non
    breaking space. We need to add it and add one to account for widow
    avoidance of the previous space.

---
+++ extensions/typo.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Plugin to monkey patch typogrify Widont plugin
+"""
+
+import unicodedata
+import re
+
+# Unicode punctation signs
+punct = u''.join(unichr(x) for x in xrange(65536) if unicodedata.category(unichr(x)) == 'Po')
+
+# If we have a space before a punctuation, we need a nbsp for it
+def replacer(m):
+    if m.group(3) != "":
+        return "%s&nbsp;%s&nbsp;%s" % (m.group(1), m.group(2), m.group(4))
+    return "%s&nbsp;%s%s" % (m.group(1), m.group(2), m.group(4))
+
+# Monkey patch Typogrify.Widont method
+@staticmethod
+def widont(text):
+    # The main modification is that we require a dot at the end of the last word.
+    widont_finder = re.compile(r"""((?:</?(?:a|em|span|strong|i|b)[^>]*>)|[^<>\s])
+                                       \s+
+                                       ([^<>\s]+)(\s*)([""" + punct + """]+
+                                       \s*
+                                       (</(a|em|span|strong|i|b)>\s*)*
+                                       ((</(p|h[1-6]|li|dt|dd)>)|$))
+                                       """, re.VERBOSE | re.UNICODE)
+    output = widont_finder.sub(replacer, text)
+    return output
+
+from typogrify import Typogrify
+Typogrify.widont = widont
+
+from hyde.plugin import Plugin
+
+class WidontPlugin(Plugin):
+    """Do nothing"""
+    pass
