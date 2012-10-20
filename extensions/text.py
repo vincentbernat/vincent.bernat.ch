dec96c87f1c526dc1756e6172afdc2323cad8c1c
Vincent Bernat <bernat@luffy.cx>
2012-10-20 18:17:50 +0200

    article: new article about network lab with KVM
    Also, my first video.

---
+++ extensions/text.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Textlinks plugin
+"""
+import re
+
+from hyde.plugin import Plugin
+
+class TextlinksPlugin(Plugin):
+    """
+    The plugin class for syntax text replacement.
+    """
+    def __init__(self, site):
+        super(TextlinksPlugin, self).__init__(site)
+
+    def begin_text_resource(self, resource, text):
+        """
+        Replace content url pattern [[dm:XXXXX]] with something
+        sensible. Currently, this is only DailyMotion stuff.
+        """
+        if not resource.uses_template:
+            return text
+        link = re.compile('\[\[([a-z]+):([^\]]*)\]\]', re.UNICODE|re.MULTILINE)
+        def replace_content(match):
+            what = match.group(1)
+            id   = match.group(2)
+
+            if what == "dailymotion":
+                return """
+<div class="lf-video-container"><div class="lf-video">
+<iframe frameborder="0" width="480" height="270"
+        src="//www.dailymotion.com/embed/video/%s"></iframe>
+</div></div>
+""" % (id,)
+            if what == "youtube":
+                return """
+<div class="lf-video-container"><div class="lf-video">
+<iframe frameborder="0" width="480" height="270" allowfullscreen
+        src="//www.youtube.com/embed/%s"></iframe>
+</div></div>
+""" % (id,)
+
+            return match.group(0)
+        text = link.sub(replace_content, text)
+        return text
