dd73c6a33f8525c9d9e08c991dbffc88232fd237
Vincent Bernat <bernat@luffy.cx>
2013-02-02 09:53:59 +0100

    Migration to the far less flexible Disqus 2012 comment system.
    New Disqus 2012 now loads comments in an iframe. We can do any special
    styling anymore and it is not possible to link to a specific
    comment. Migration is mandatory since the old Disqus will be not be
    available from March 1st.

--- content/media/js/luffy.comments.js
+++ content/media/js/luffy.comments.js
@@ -24,50 +24,10 @@ luffy.comments = function() {
 	.show()			// Show because JS is enabled
 	.click(load);		// Load on click
 
-    // If this is Googlebot, load comments right now (maybe comments will be indexed)
-    if (/googlebot/i.test(navigator.userAgent)) load();
-
     /* Load on scroll to bottom */
     $(window).scroll(function() {
 	var bottom = $(window).scrollTop() + $(window).height();
 	var top = el.offset().top - 300;
 	if (bottom >= top) load();
-    });
-
-    /* Display comments when the current location point to a comment */
-    var display_comment = function(comment) {
-	/* Highlight the target comment */
-	var style = "#dsq-comment-" + comment + " .dsq-comment-header { " +
-	    "background-color: #FBE686; background-image: none !important; " +
-	    "border:3px solid #FBC586; }";
-	$("head").append("<style>" + style + "</style>");
-
-	/* Scroll to comment(s) */
-	if ('object' == typeof window.scroll ||
-	    'function' == typeof window.scroll) {
-	    var el = $("#dsq-comment-" + comment);
-	    window.scroll(0, (el.length?el:$("#disqus_thread")).offset().top);
-	}
-
-	load();			// Make Disqus load if needed
-    }
-
-    /* Display anchored comment */
-    if (location.hash.match("^#comment-[0-9]+")) {
-	var comment = location.hash.substr(9);
-	display_comment(comment);
-    }
-
-    /* Also display them when we click on a link to a comment to the
-       same page */
-    $("article a").filter(function (index) {
-	return (this.hostname == location.hostname) &&
-	    (this.pathname == location.pathname ||
-	     "/" + this.pathname == location.pathname) &&
-	    (typeof this.hash !== 'undefined') &&
-	    (this.hash.match("^#comment-[0-9]+"));
-    }).click(function() {
-	var comment = this.hash.substr(9);
-	display_comment(comment);
     });
 };
