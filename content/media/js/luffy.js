9993e03920dc3adebc8b3b5264e1a3dc7f3dc81e
Vincent Bernat <bernat@luffy.cx>
2013-08-17 21:22:47 +0200

    js: don't log errors to GA

--- content/media/js/luffy.js
+++ content/media/js/luffy.js
@@ -6,16 +6,6 @@ combine:
     where: top
     remove: yes
 ---
-/* Log errors to Google Analytics.
-   See: http://www.thetaboard.com/blog/client-side-error-logging-with-google-analytics
-*/
-if (typeof(_gaq) !== 'undefined') {
-    window.onerror = function(message, file, line) {
-	var sFormattedMessage = '[' + file + ' (' + line + ')] ' + message;
-	_gaq.push(['_trackEvent', 'Exceptions', 'Application',
-		   sFormattedMessage, null, true]);
-    }
-}
 
 $(function() {
     luffy.mathjax();
