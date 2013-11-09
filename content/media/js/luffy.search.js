cd935aa3d9be846063fac6c628a8deaf59138b66
Vincent Bernat <bernat@luffy.cx>
2013-11-09 14:00:36 +0100

    css: rework input search box

--- content/media/js/luffy.search.js
+++ content/media/js/luffy.search.js
@@ -2,21 +2,6 @@
 
 var luffy = luffy || {};
 luffy.search = function() {
-    /* Clicking in the search box change the background to white */
-    $("#lf-search-query").focus(function() {
-	$(this)
-	    .css({"background-color": "white",
-		  color: "black"});
-
-    });
-    $("#lf-search-query").blur(function() {
-	if ($(this).val() === "") {
-	    $(this)
-		.css({"background-color": "#666",
-		      color: "#ccc"});
-	}
-    });
-
     var panel = $("#lf-search-results");
     var results = $("#lf-search-results-results")
 
