af26e4ddf3964b15d74bb052bbcd9c017f028bb1
Vincent Bernat <vincent@bernat.im>
2016-04-03 19:07:37 +0200

    js: remove use of jQuery for MathJax loading

--- content/media/js/luffy.mathjax.js
+++ content/media/js/luffy.mathjax.js
@@ -4,9 +4,10 @@ var luffy = luffy || {};
 luffy.mathjax = function() {
     var delim = "·"; // It's "middle dot"
     var mathjax = "http://cdn.mathjax.org/mathjax/latest/MathJax.js";
+    var main = document.getElementById("lf-main");
 
     /* Don't load if we don't find the delimiter. */
-    if ($("#lf-main").text().indexOf(delim) === -1) return;
+    if ((main.innerText || main.textContent || "").indexOf(delim) === -1) return;
 
     /* Otherwise, load. Input: TeX/AMS. Output: HTML+CSS.*/
     $script(mathjax + "?config=TeX-AMS_HTML-full&delayStartupUntil=configured",
