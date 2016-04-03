3de4db201f569dcac377cf5bdd6e737d5395d3ab
Vincent Bernat <vincent@bernat.im>
2016-04-03 18:28:36 +0200

    css: replace cssprefixer by postcss with autoprefix plugin
    cssprefixer is deprecated and doesn't handle @keyframes keyword correctly.

--- extensions/css.py
+++ extensions/css.py
@@ -3,15 +3,31 @@
 CSS plugins
 """
 
+import subprocess
 from hyde.plugin import Plugin
-import cssprefixer
+
 
 class CSSPrefixerPlugin(Plugin):
     """Run CSS prefixer"""
     def text_resource_complete(self, resource, text):
-        if not resource.source_file.kind in ("less", "css"):
+        if resource.source_file.kind not in ("less", "css"):
             return
-        return cssprefixer.process(text,
-                                   debug=False,
-                                   minify=True).replace("display:inline-block;",
-                                                        "display:inline-block;zoom:1;*display:inline;")
+        p = subprocess.Popen(['nodejs', '-e', """
+var autoprefixer = require('autoprefixer');
+var postcss = require('postcss');
+var input = '';
+
+process.stdin.setEncoding('utf8')
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+  if (chunk) {
+    input += chunk;
+  }
+});
+process.stdin.on('end', function() {
+  var output = postcss([autoprefixer]).process(input).css.toString();
+  process.stdout.write(output);
+});
+        """], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        stdout, _ = p.communicate(text.encode('utf-8'))
+        return stdout.decode('utf-8')
