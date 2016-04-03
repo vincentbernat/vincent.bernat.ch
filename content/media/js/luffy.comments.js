79490a98ec94cbd83709afea42e888d82cf1da0c
Vincent Bernat <vincent@bernat.im>
2016-04-04 00:13:03 +0200

    comments: rework how Disqus is loaded
    Give more explanations and remove the hiddeous button that was used.

--- content/media/js/luffy.comments.js
+++ content/media/js/luffy.comments.js
@@ -2,29 +2,34 @@
 
 var luffy = luffy || {};
 luffy.comments = function() {
-    var el = document.getElementById("lf-disqus");	// Don't do anything if there is no comment
+    // Don't do anything if there is no comment
+    var el = document.getElementById("disqus_thread");
     if (el === null || el === undefined) return;
 
-    /* Function to load Disqus */
+    // Load Disqus on click
     var load = function() {
 	var done = false;
-	return function() {
+	return function(e) {
+            e.preventDefault();
 	    if (done) return;
 	    done = true;	// Don't want to load twice.
-	    el.innerHTML = 'Loading/Chargement...';
             var src = 'https://' + disqus_shortname + '.disqus.com/embed.js';
-	    $script(src, function() {
-		el.style.display = 'none';
-	    });
+	    $script(src, function() {});
+            return;
 	}
     }();
 
-    el.style.display = 'block';
-    el.addEventListener('click', load, false);
+    var links = el.querySelectorAll('a[href=""]'), i;
+    for (i = 0; i < links.length; i++) {
+        links[i].addEventListener('click', load, false);
+    }
 
     /* Load if we have an anchor */
     if (location.hash.match("^#comment-[0-9]+")) {
         load();
     }
 
+    // Display introduction text
+    el.children[1].style.display = 'none';
+    el.children[2].style.display = 'block';
 };
