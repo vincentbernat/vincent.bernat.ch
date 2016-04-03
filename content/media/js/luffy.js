646e169cab67fd5bfe5366ec7f67ed4a5b802720
Vincent Bernat <vincent@bernat.im>
2016-04-03 19:20:25 +0200

    js: ensure errors in a module doesn't propagate outside

--- content/media/js/luffy.js
+++ content/media/js/luffy.js
@@ -7,7 +7,14 @@ combine:
     remove: yes
 ---
 
-luffy.mathjax();
-luffy.effects();
-luffy.comments();
-luffy.gallery();
+var scripts = [luffy.mathjax,
+               luffy.effects,
+               luffy.comments,
+               luffy.gallery];
+for (var i = 0; i < scripts.length; i++) {
+    try {
+        scripts[i]();
+    } catch (e) {
+        (console.error || console.log).call(console, e.stack || e);
+    }
+}
