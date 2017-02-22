35c0d07004e2c669f5d443c740786bba45c9eff9
Vincent Bernat <vincent@bernat.im>
2017-02-22 21:36:49 +0100

    extensions: correctly handle french typography
    We use narrow non-breaking space before double punctuation and similar
    other tricks. With this change, we don't need a modified version of the
    widow plugin.

--- extensions/typo.py
+++ extensions/typo.py
@@ -1,38 +1,37 @@
 # -*- coding: utf-8 -*-
 """
-Plugin to monkey patch typogrify Widont plugin
+Plugin to handle some typography stuff (monkey-patch typogrify).
 """
 
-import unicodedata
 import re
-
-# Unicode punctation signs
-punct = u''.join(unichr(x) for x in xrange(65536) if unicodedata.category(unichr(x)) == 'Po')
-
-# If we have a space before a punctuation, we need a nbsp for it
-def replacer(m):
-    if m.group(3) != "":
-        return "%s&nbsp;%s&nbsp;%s" % (m.group(1), m.group(2), m.group(4))
-    return "%s&nbsp;%s%s" % (m.group(1), m.group(2), m.group(4))
-
-# Monkey patch Typogrify.Widont method
-def widont(text):
-    # The main modification is that we require a dot at the end of the last word.
-    widont_finder = re.compile(r"""((?:</?(?:a|em|span|strong|i|b)[^>]*>)|[^<>\s])
-                                       \s+
-                                       ([^<>\s]+)(\s*)([""" + punct + """]+
-                                       \s*
-                                       (</(a|em|span|strong|i|b)>\s*)*
-                                       ((</(p|h[1-6]|li|dt|dd)>)|$))
-                                       """, re.VERBOSE | re.UNICODE)
-    output = widont_finder.sub(replacer, text)
-    return output
-
-from typogrify import filters
-filters.widont = widont
 
 from hyde.plugin import Plugin
 
-class WidontPlugin(Plugin):
-    """Do nothing"""
-    pass
+
+class FrenchPunctuationPlugin(Plugin):
+    """Monkey-patch typogrify to correctly handle french punctuation."""
+
+    NNBSP = u'&#x202f;'
+
+    def __init__(self, site):
+        # Patch typogrify
+        from typogrify import filters
+        original_applyfilters = filters.applyfilters
+        filters.applyfilters = lambda text: self.frenchpunct(original_applyfilters(text))
+
+    def frenchpunct(self, text):
+        tag_pattern = '</?\w+((\s+\w+(\s*=\s*(?:".*?"|\'.*?\'|[^\'">\s]+))?)+\s*|\s*)/?>'
+        space_before_punct_finder = re.compile(r"""(\s|&nbsp;)([:;!\?%»])""", re.UNICODE)
+        space_after_punct_finder = re.compile(r"""([«])(\s|&nbsp;)""", re.UNICODE)
+        intra_tag_finder = re.compile(r'(?P<prefix>(%s)?)(?P<text>([^<]*))(?P<suffix>(%s)?)' % (tag_pattern, tag_pattern))
+
+        def _space_process(groups):
+            prefix = groups.group('prefix') or ''
+            text = groups.group('text')
+            text = space_before_punct_finder.sub(self.NNBSP + r"\2", text)
+            text = space_after_punct_finder.sub(r"\1" + self.NNBSP, text)
+            suffix = groups.group('suffix') or ''
+            return prefix + text + suffix
+
+        output = intra_tag_finder.sub(_space_process, text)
+        return output
