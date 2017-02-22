d3a7578a459dc95daa6add75a7c10a8cff71dbcb
Vincent Bernat <vincent@bernat.im>
2017-02-22 22:29:38 +0100

    extensions: also use nnbsp before € sign

--- extensions/typo.py
+++ extensions/typo.py
@@ -25,7 +25,7 @@ class FrenchPunctuationPlugin(Plugin):
         space_after_punct_finder = re.compile(r"""([«])(\s|&nbsp;)""", re.UNICODE)
         space_between_figures_finder = re.compile(r"""([0-9]|^)(\s|&nbsp;)([0-9]+(?:\W|$))""", re.UNICODE)
         version_number_finder = re.compile(r"""(\b[A-Z][a-zA-Z]+)(\s|&nbsp;)([0-9]+(?:\W|$))""", re.UNICODE)
-        si_unit_finder = re.compile(ur"""(\b[0-9,.]+)( |&nbsp;)(\w)""", re.UNICODE)  # Cheating, nbsp already here...
+        si_unit_finder = re.compile(ur"""(\b[0-9,.]+)( |&nbsp;)(\w|€)""", re.UNICODE)  # Cheating, nbsp already here...
         intra_tag_finder = re.compile(r'(?P<prefix>(%s)?)(?P<text>([^<]*))(?P<suffix>(%s)?)' % (tag_pattern, tag_pattern))
 
         def _space_process(groups):
