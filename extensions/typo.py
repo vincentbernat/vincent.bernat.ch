0de4e3328d8d834c8f4a8744b3d0ae022b6d9875
Vincent Bernat <vincent@bernat.im>
2017-02-22 21:57:31 +0100

    extensions: also use a thin non-breakable space before numbers
    This is done between two numbers or if a number is like a version
    number (the previous word starts with a capital). This seems to work
    fine.

--- extensions/typo.py
+++ extensions/typo.py
@@ -23,6 +23,8 @@ class FrenchPunctuationPlugin(Plugin):
         tag_pattern = '</?\w+((\s+\w+(\s*=\s*(?:".*?"|\'.*?\'|[^\'">\s]+))?)+\s*|\s*)/?>'
         space_before_punct_finder = re.compile(r"""(\s|&nbsp;)([:;!\?%»])""", re.UNICODE)
         space_after_punct_finder = re.compile(r"""([«])(\s|&nbsp;)""", re.UNICODE)
+        space_between_figures_finder = re.compile(r"""([0-9]|^)(\s|&nbsp;)([0-9]+\W)""", re.UNICODE)
+        version_number_finder = re.compile(r"""(\b[A-Z][a-zA-Z]+)(\s|&nbsp;)([0-9]+\W)""", re.UNICODE)
         intra_tag_finder = re.compile(r'(?P<prefix>(%s)?)(?P<text>([^<]*))(?P<suffix>(%s)?)' % (tag_pattern, tag_pattern))
 
         def _space_process(groups):
@@ -30,6 +32,8 @@ class FrenchPunctuationPlugin(Plugin):
             text = groups.group('text')
             text = space_before_punct_finder.sub(self.NNBSP + r"\2", text)
             text = space_after_punct_finder.sub(r"\1" + self.NNBSP, text)
+            text = space_between_figures_finder.sub(r"\1" + self.NNBSP + r"\3", text)
+            text = version_number_finder.sub(r"\1" + self.NNBSP + r"\3", text)
             suffix = groups.group('suffix') or ''
             return prefix + text + suffix
 
