459790af9f2caf346cdb660023a59b64d42f9ad5
Vincent Bernat <vincent@bernat.im>
2017-02-24 22:12:13 +0100

    images: simplify image captions
    There was some unneeded complexity:
    
     - the caption doesn't overlap the image anymore and we don't bother
       with the very small reflow that could happen if the caption has
       several lines
    
     - the JS code to hide the caption was here because the caption could be
       overlapping the image, which is not the case anymore (and it could
       have been done with CSS instead)

--- content/media/js/luffy.captions.js
+++ content/media/js/luffy.captions.js
@@ -9,20 +9,10 @@ luffy.captions = function() {
         if (el.parentNode.tagName.toLowerCase() === "a") {
             el = el.parentNode;
         }
-        (function() {
-            var caption = document.createElement("div");
-            caption.className = "lf-caption";
-            caption.innerText = caption.textContent = title;
-            el.parentNode.appendChild(caption);
-            el.parentNode.className += " lf-captioned";
-            if (el.addEventListener) {
-                el.addEventListener("mouseover", function() {
-                    caption.style.display = 'none';
-                });
-                el.addEventListener("mouseout", function() {
-                    caption.style.display = 'block';
-                });
-            }
-        })();
+        var caption = document.createElement("div");
+        caption.className = "lf-caption";
+        caption.innerText = caption.textContent = title;
+        el.parentNode.appendChild(caption);
+        el.parentNode.className += " lf-captioned";
     }
 };
