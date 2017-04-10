d0b3e7c7ad4391216a11d26eb68330d7933437e3
Vincent Bernat <vincent@bernat.im>
2017-04-10 07:12:51 +0200

    js: switch from Mathjax to KaTeX
    It is faster, smaller and good enough for us. We load it directly in the
    pages where it is needed. This removes a bunch of JS.

--- content/media/js/luffy.mathjax.js
+++ content/media/js/luffy.mathjax.js
@@ -1,31 +1,8 @@
-/* Load nathjax if needed */
-
 var luffy = luffy || {};
 luffy.mathjax = function() {
-    var delim = "·"; // It's "middle dot"
-    var mathjax = "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js";
-    var main = document.getElementById("lf-main");
-
-    /* Don't load if we don't find the delimiter. */
-    if ((main.innerText || main.textContent || "").indexOf(delim) === -1) return;
-
-    /* Otherwise, load. Input: TeX/AMS. Output: HTML+CSS.*/
-    $script(mathjax + "?config=TeX-AMS_CHTML&delayStartupUntil=configured&noContrib",
-	    function() {
-		  /* Add more configuration stuff */
-		  MathJax.Hub.Config({
-		      elements: ["lf-main"], // Only process part of the page.
-		      showMathMenu: false,
-		      tex2jax: {
-			  inlineMath: [ [delim,delim] ],
-			  displayMath: [ [delim + delim, delim + delim] ],
-			  processEnvironments: false
-		      },
-		      "CommonHTML": {
-			  scale: 97,
-		      }
-		  });
-		  MathJax.Hub.Configured();
-		  MathJax.Hub.Startup.onload();
-	    });
+  // KaTeX rendering. Assuming it is loaded.
+  renderMathInElement(document.body, {
+    delimiters: [{left: "··", right: "··", display: true},
+                 {left: "·", right: "·", display: false}]
+  });
 };
