99e9dca2b8ea992a5c127a0fb3f142c816d4aff2
Vincent Bernat <vincent@bernat.im>
2017-04-15 21:24:01 +0200

    js: don't render math stuff when KaTeX is not loaded

--- content/media/js/luffy.mathjax.js
+++ content/media/js/luffy.mathjax.js
@@ -1,6 +1,7 @@
 var luffy = luffy || {};
 luffy.mathjax = function() {
   // KaTeX rendering. Assuming it is loaded.
+  if (typeof window.renderMathInElement !== "function") return;
   renderMathInElement(document.body, {
     delimiters: [{left: "··", right: "··", display: true},
                  {left: "·", right: "·", display: false}]
