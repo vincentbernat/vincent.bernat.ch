4798bba59a1717c3578f855fa450c57abe01402e
Vincent Bernat <vincent@bernat.im>
2017-12-09 23:58:56 +0100

    layout: use shorter dates
    Day is not really need, only keep month and year.

--- extensions/jinja.py
+++ extensions/jinja.py
@@ -2,4 +2,4 @@ from babel.dates import format_date
 
 
 def humandate(dt, locale='en'):
-    return format_date(dt, locale=locale, format='long')
+    return format_date(dt, 'MMMM yyyy', locale=locale)
