7cfd14231dff2f16c0fc5fafb9a4da79f175328f
Vincent Bernat <vincent@bernat.im>
2018-01-26 09:32:36 +0100

    js: don't try to be too smart with hls.js
    Just load it and let it handle the situation.

--- content/media/js/luffy.videos.js
+++ content/media/js/luffy.videos.js
@@ -5,28 +5,10 @@ luffy.s.push(function() {
     var videoSources = document.querySelectorAll(".lf-video source[type='application/vnd.apple.mpegurl']");
     if (videoSources.length == 0) return;
 
-    // Check support for MSE to use with hls.js (code stolen from hls.js)
-    if ((function() {
-        var mediaSource = window.MediaSource || window.WebKitMediaSource;
-        var sourceBuffer = window.SourceBuffer || window.WebKitSourceBuffer;
-        var isTypeSupported = mediaSource &&
-            typeof mediaSource.isTypeSupported === 'function' &&
-            mediaSource.isTypeSupported('video/mp4; codecs="avc1.42E01E,mp4a.40.2"');
-        var sourceBufferValidAPI = !sourceBuffer ||
-            sourceBuffer.prototype &&
-            typeof sourceBuffer.prototype.appendBuffer === 'function' &&
-            typeof sourceBuffer.prototype.remove === 'function';
-        return (!isTypeSupported || !sourceBufferValidAPI);
-    })()) return;
-
-    // We could also detect native HLS support but we don't want to
-    // use native for broken implementation like Android. We could
-    // detect native support on Safari only, but meeeh.
-
-    // Only load hls.js if we really need it
     var script = document.querySelector('script[data-name="hls.js"]');
     script.onload = function() {
         if (!Hls.isSupported()) return;
+
         [].forEach.call(videoSources, function(videoSource) {
             var m3u8 = videoSource.src,
                 once = false,
