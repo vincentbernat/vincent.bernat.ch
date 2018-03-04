3427522e793a15463487999eed0a99e415705050
Vincent Bernat <vincent@bernat.im>
2018-03-04 07:49:47 +0100

    media: don't expose S3 backup URL for video
    Many spiders browse that. May be expensive.

--- extensions/media.py
+++ extensions/media.py
@@ -34,7 +34,6 @@ class MoreMediaPlugin(Plugin):
         controls preload="none">
   <source src="{m3u8}" type="application/vnd.apple.mpegurl">
   <source src="https://luffy-video.sos-ch-dk-2.exo.io/{id}/progressive.mp4" type='video/mp4; codecs="avc1.4d401f, mp4a.40.2"'>
-  <source src="https://luffy-video.s3.eu-central-1.amazonaws.com/{id}/progressive.mp4" type='video/mp4; codecs="avc1.4d401f, mp4a.40.2"'>
  </video>
 </div></div>
 """.format(id=match.group(1), poster=poster, m3u8=m3u8)
