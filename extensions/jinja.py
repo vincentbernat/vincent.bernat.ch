7108e67171d1073a7720d433abe8f0d9bc291102
Vincent Bernat <vincent@bernat.im>
2018-04-02 12:10:06 +0200

    layout: add suggestions based on the category

--- extensions/jinja.py
+++ extensions/jinja.py
@@ -8,3 +8,23 @@ def humandate(dt, locale='en', format=None):
         return format_date(dt, 'MMMM yyyy', locale=locale)
     else:
         return format_date(dt, format=format, locale=locale)
+
+
+def samecategory(resource, attribute, skip=0):
+    """Returns the next resource in the same category using the provided
+    attribute to iterate over resources."""
+    tags = {t.name
+            for t in getattr(resource, 'tags', [])
+            if t.name != 'outdated'}
+    candidate = resource
+    while candidate:
+        candidate = getattr(candidate, attribute, None)
+        if skip:
+            skip -= 1
+            continue
+        ctags = {t.name for t in getattr(candidate, 'tags', [])}
+        if 'outdated' in ctags:
+            continue
+        if len(tags & ctags) > 0:
+            break
+    return candidate
