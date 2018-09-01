296a92aa60589ebbc2815340800cbf44d7db7397
Vincent Bernat <vincent@bernat.ch>
2018-09-01 11:01:06 +0200

    cdn: use complete URL for media.luffy.cx
    CSP doesn't support URL without scheme.

--- extensions/cdn.py
+++ extensions/cdn.py
@@ -14,17 +14,17 @@ from hyde.site import Site
 from fswrap import FS, File, Folder
 
 # Serve everything from CDN except content of files/ and videos/
-NOCDN="//media.luffy.cx/"
+NOCDN="https://media.luffy.cx/"
 def decorate_media_url(media_url):
     @wraps(media_url)
     def wrapper(site, path, safe=None):
         if path.startswith("files/") or path.startswith("videos/"):
             # Don't use CDN for those big files
-            path = Folder(NOCDN).child(path).replace(os.sep, '/').encode("utf-8")
             if safe is not None:
-                return quote(path, safe)
+                path = quote(path, safe)
             else:
-                return quote(path)
+                path = quote(path)
+            return Folder(NOCDN).child(path).encode("utf-8")
         return media_url(site, path, safe)
     return wrapper
 
