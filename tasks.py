6aa0fa78f0d468b4c566abfc1f04c767fe10eff3
Vincent Bernat <vincent@bernat.ch>
2019-12-07 13:17:40 +0100

    tasks: move from fabric to invoke

--- tasks.py
+++ tasks.py
@@ -1,49 +1,61 @@
 # -*- coding: utf-8 -*-
-from fabric.api import *
-from fabric.contrib.console import confirm, prompt
+from invoke import task
 
 import os
-import shutil
+import sys
 import time
-import json
-import glob
-import hashlib
 import yaml
 import csv
 import re
-import operator
 import datetime
+import subprocess
 
-env.shell = "/bin/sh -c"
-env.command_prefixes = ['export PATH=$HOME/.virtualenvs/hyde/bin:$PATH']
+os.environ["PATH"] = os.path.expanduser('~/.virtualenvs/hyde/bin') \
+    + os.pathsep + os.environ["PATH"]
 
 conf = "site-production.yaml"
-media = yaml.safe_load(file(conf))['media_url']
+media = yaml.safe_load(open(conf))['media_url']
 hosts = ["web03.luffy.cx", "web04.luffy.cx"]
 
-def _hyde(args):
-    return local('hyde -x %s' % args)
+
+def confirm(question, default=False):
+    if default:
+        suffix = "Y/n"
+    else:
+        suffix = "y/N"
+    while True:
+        response = input("{0} [{1}] ".format(question, suffix))
+        response = response.lower().strip()  # Normalize
+        # Default
+        if not response:
+            return default
+        if response in ["y", "yes"]:
+            return True
+        if response in ["n", "no"]:
+            return False
+        err = "I didn't understand you. Please specify '(y)es' or '(n)o'."
+        print(err, file=sys.stderr)
+
 
 @task
-def regen():
-    """Regenerate dev content"""
-    local('rm -rf deploy')
-    gen()
-
-@task
-def gen():
+def gen(c):
     """Generate dev content"""
-    _hyde('gen')
+    c.run('hyde -x gen')
+
+@task(post=[gen])
+def regen(c):
+    """Regenerate dev content"""
+    c.run('rm -rf deploy')
 
 @task
-def serve():
+def serve(c):
     """Serve dev content"""
-    _hyde('serve -a 0.0.0.0')
+    c.run('hyde -x serve -a 0.0.0.0')
 
 @task
-def sprite():
+def sprite(c):
     """Regenerate sprites"""
-    local(" ".join(["./node_modules/svg-sprite/bin/svg-sprite.js",
+    c.run(" ".join(["./node_modules/svg-sprite/bin/svg-sprite.js",
                     "--shape-spacing-padding=2px",
                     "--css",                         # CSS mode
                     "--css-bust=false",              # No cache busting
@@ -59,7 +71,7 @@ def sprite():
 
 
 @task
-def screenshots():
+def screenshots(c):
     """Generate screenshots"""
     now = time.asctime().replace(" ", "-")
     os.makedirs("screenshots/{now}".format(now=now))
@@ -70,7 +82,7 @@ def screenshots():
                 "en/blog/2011-thinkpad-edge-11.html",
                 "en/blog/2017-ipv6-route-lookup-linux.html"]:
         for width in [320, 600, 1024, 1280, 1900]:
-            local("chromium "
+            c.run("chromium "
                   "--headless "
                   "--hide-scrollbars "
                   "--screenshot "
@@ -130,7 +142,7 @@ EOF
 # http://www.redwirez.com/pcalc.jsp to convert dB to percents).
 
 @task
-def upload_videos(video=None):
+def upload_videos(c, video=None):
     """Upload a transcoded video."""
     path = 'content/media/videos'
     for directory in os.listdir(path):
@@ -140,28 +152,28 @@ def upload_videos(video=None):
             continue
         # Upload
         for host in hosts:
-            local("rsync -a {directory}/ {host}:/data/webserver/media.luffy.cx/videos/{short}/".format(
+            c.run("rsync -a {directory}/ {host}:/data/webserver/media.luffy.cx/videos/{short}/".format(
                 host=host,
                 short=directory,
                 directory=os.path.join(path, directory)))
         # Copy poster and index.m3u8
-        local("cp {directory}/poster.jpg content/media/images/posters/{short}.jpg".format(
+        c.run("cp {directory}/poster.jpg content/media/images/posters/{short}.jpg".format(
             short=directory,
             directory=os.path.join(path, directory)))
-        local("cp {directory}/index.m3u8 content/media/videos/{short}.m3u8".format(
+        c.run("cp {directory}/index.m3u8 content/media/videos/{short}.m3u8".format(
             short=directory,
             directory=os.path.join(path, directory)))
 
 
 @task
-def updatefonts():
+def updatefonts(c):
     """Download latest Merriweather fonts"""
-    with lcd('content/media/fonts'):
-        local('wget -O merriweather.zip https://google-webfonts-helper.herokuapp.com/api/fonts/'
+    with c.cd('content/media/fonts'):
+        c.run('wget -O merriweather.zip https://google-webfonts-helper.herokuapp.com/api/fonts/'
               'merriweather\\?download=zip\\&subsets=latin,latin-ext'
               '\\&variants=300,300italic\\&formats=woff,woff2')
-        local('unzip merriweather.zip \\*.woff')
-        local('rm merriweather.zip')
+        c.run('unzip merriweather.zip \\*.woff')
+        c.run('rm merriweather.zip')
         for f in os.listdir('content/media/fonts'):
             if not f.startswith('merriweather-v'):
                 continue
@@ -171,30 +183,29 @@ def updatefonts():
             target = target.replace('-300.', '.')
             target = target.replace('-300', '-')
             # Patch
-            local('ttx -o - {} '
+            c.run('ttx -o - {} '
                   '| xmlstarlet ed '
                   '     -u /ttFont/post/underlineThickness/@value -v 75'
                   '> {}.ttx'.format(f, f))
-            local('ttx -o {} --flavor=woff {}.ttx'.format(target, f))
-            local('ttx -o {}2 --flavor=woff2 {}.ttx'.format(target, f))
-            local('rm {}.ttx'.format(f))
-            local('rm {}'.format(f))
+            c.run('ttx -o {} --flavor=woff {}.ttx'.format(target, f))
+            c.run('ttx -o {}2 --flavor=woff2 {}.ttx'.format(target, f))
+            c.run('rm {}.ttx'.format(f))
+            c.run('rm {}'.format(f))
 
 
 @task
-def linkcheck(remote='yes', verbose='no'):
+def linkcheck(c, remote='yes', verbose='no'):
     """Check links"""
-    with settings(warn_only=True):
-        result = local("linkchecker -f ./linkcheckerrc {} {}".format(
-            verbose == 'yes' and '--verbose' or '',
-            remote == 'yes' and
-            'https://vincent.bernat.ch/' or
-            'http://localhost:8080/'))
+    result = c.run("linkchecker -f ./linkcheckerrc {} {}".format(
+        verbose == 'yes' and '--verbose' or '',
+        remote == 'yes' and
+        'https://vincent.bernat.ch/' or
+        'http://localhost:8080/'), warn=True)
     if result.failed:
-        fixlinks()
+        fixlinks(c)
 
 @task
-def fixlinks():
+def fixlinks(c):
     """Try to fix links"""
     fp = open("linkchecker-out.csv")
     reader = csv.DictReader(filter(lambda row: row[0]!='#', fp), delimiter=';')
@@ -214,22 +225,22 @@ def fixlinks():
                        not row['urlname'].startswith('http:') and "s" or "",
                        year, row['urlname'])}
         while True:
-            print """
+            print("""
 URL:     {urlname}
 Source:  {parentname}
 Result:  {result}
 Warning: {warningstring}
-Info:    {infostring}""".format(**row)
-            print """
+Info:    {infostring}""".format(**row))
+            print("""
 (c) Continue
 (b) Browse {urlname}
 (p) Browse {parentname}
 (r) Replace by your own URL
-(q) Quit""".format(**row)
+(q) Quit""".format(**row))
             valid = "cbprq"
             for a in archive:
-                print "({}) Browse {}".format(a, archive[a])
-                print "({}) Replace by {}".format(a.upper(), archive[a])
+                print("({}) Browse {}".format(a, archive[a]))
+                print("({}) Replace by {}".format(a.upper(), archive[a]))
                 valid += a
                 valid += a.upper()
             if 'Redirected' in row['infostring']:
@@ -237,35 +248,37 @@ Info:    {infostring}""".format(**row)
                                flags=re.DOTALL)
                 if mo:
                     redirected = mo.group(1)
-                    print "(R) Replace by {}".format(redirected)
+                    print("(R) Replace by {}".format(redirected))
                     valid += 'R'
-            print
-            ans = prompt("Command?", validate=r"[{}]".format(valid))
+            print()
+            ans = input("Command? ")
+            if ans not in valid:
+                continue
             if ans == "c":
                 break
             elif ans == "q":
                 return
             elif ans == "r":
-                url = prompt("URL?")
-                local("git grep -Fl '{}' | xargs -r sed -i 's, {}, {},g'".format(
+                url = input("URL? ")
+                c.run("git grep -Fl '{}' | xargs -r sed -i 's, {}, {},g'".format(
                     row['urlname'], row['urlname'], url))
                 break
             elif ans == "b":
-                local("x-www-browser {}".format(row['urlname']))
+                c.run("x-www-browser {}".format(row['urlname']))
             elif ans == "p":
-                local("x-www-browser {}".format(row['parentname']))
+                c.run("x-www-browser {}".format(row['parentname']))
             elif ans == "R":
-                local("git grep -Fl '{}' | xargs -r sed -i 's, {}, {},g'".format(
+                c.run("git grep -Fl '{}' | xargs -r sed -i 's, {}, {},g'".format(
                     row['urlname'], row['urlname'], redirected))
                 break
             else:
                 found = False
                 for a in archive:
                     if ans == a:
-                        local("x-www-browser {}".format(archive[a]))
+                        c.run("x-www-browser {}".format(archive[a]))
                         break
                     elif ans == a.upper():
-                        local("git grep -Fl '{}' | xargs -r sed -i 's, {}, {},g'".format(
+                        c.run("git grep -Fl '{}' | xargs -r sed -i 's, {}, {},g'".format(
                             row['urlname'], row['urlname'], archive[a]))
                         found = True
                         break
@@ -274,36 +287,36 @@ Info:    {infostring}""".format(**row)
         seen[row['urlname']] = True
 
 @task
-def build():
+def build(c):
     """Build production content"""
-    local("[ $(git rev-parse --abbrev-ref HEAD) = latest ]")
-    with lcd("content/en"):
-        local("! git grep -Pw '(?i:obviously|basically|simply|clearly|everyone knows|turns out)' \\*.html")
-    local("rm -rf .final/*")
-    local("yarn install --frozen-lockfile")
-    _hyde('gen -c %s' % conf)
-    with lcd(".final"):
+    c.run("[ $(git rev-parse --abbrev-ref HEAD) = latest ]")
+    with c.cd("content/en"):
+        c.run("! git grep -Pw '(?i:obviously|basically|simply|clearly|everyone knows|turns out)' \\*.html")
+    c.run("rm -rf .final/*")
+    c.run("yarn install --frozen-lockfile")
+    c.run('hyde -x gen -c %s' % conf)
+    with c.cd(".final"):
         # Fix HTML (<source> is an empty tag)
-        local(r"find . -name '*.html' -print0"
+        c.run(r"find . -name '*.html' -print0"
               r"| xargs -0 sed -i 's+\(<source[^>]*>\)</source>+\1+g'")
-        local(r"find . -name '*.html' -print0"
+        c.run(r"find . -name '*.html' -print0"
               r"| xargs -0 sed -i 's+\(<track[^>]*>\)</track>+\1+g'")
         # Optimize SVG (consider using svgcleaner instead, svgo is a bit fragile)
-        local("find media/images -type f -name '*.svg'"
+        c.run("find media/images -type f -name '*.svg'"
               "| sed 's+/[^/]*$++' | sort | uniq"
               "| grep -Ev '^media/images/(l|obj)(/|$)'"
               "| xargs -n1 -P3 ../node_modules/svgo/bin/svgo --quiet --disable=mergePaths")
-        local("find media/images -type f -name '*.svg'"
+        c.run("find media/images -type f -name '*.svg'"
               "| grep -Ev '^media/images/(l|obj)(/|$)'"
               "| xargs -n1 -P3 sed -i 's/style=.marker:none. //g'")
         # Optimize JPG
-        local("find media/images -type f -name '*.jpg' -print0"
+        c.run("find media/images -type f -name '*.jpg' -print0"
               " | xargs -0 -n10 -P4 jpegoptim --max=84 --strip-all")
-        local("find media/images -type f -name '*.jpg' -print0"
+        c.run("find media/images -type f -name '*.jpg' -print0"
               " | xargs -0 -n1 -I'{}' -P4 jpegtran -optimize -progressive "
               "                                    -copy none -outfile '{}' '{}'")
         # Optimize PNG
-        local("find media/images -type f -name '*.png' -print0"
+        c.run("find media/images -type f -name '*.png' -print0"
               " | xargs -0 -n10 -P4 optipng -quiet")
         # Subset fonts. Nice tool to quickly look at the result:
         #  http://torinak.com/font/lsfont.html
@@ -311,10 +324,10 @@ def build():
             options = " ".join(["--name-IDs+=0,4,6",
                                 "--text-file=../glyphs-{}.txt".format(glyphs),
                                 "--no-hinting --desubroutinize"])
-            local("pyftsubset media/fonts/{}.woff  --flavor=woff --with-zopfli {}".format(font, options))
-            local("pyftsubset media/fonts/{}.woff2 --flavor=woff2 {}".format(font, options))
-            local("mv media/fonts/{}.subset.woff  media/fonts/{}.woff".format(font, font))
-            local("mv media/fonts/{}.subset.woff2 media/fonts/{}.woff2".format(font, font))
+            c.run("pyftsubset media/fonts/{}.woff  --flavor=woff --with-zopfli {}".format(font, options))
+            c.run("pyftsubset media/fonts/{}.woff2 --flavor=woff2 {}".format(font, options))
+            c.run("mv media/fonts/{}.subset.woff  media/fonts/{}.woff".format(font, font))
+            c.run("mv media/fonts/{}.subset.woff2 media/fonts/{}.woff2".format(font, font))
         subset('iosevka-term', 'monospace')
         subset('merriweather', 'regular')
         subset('merriweather-italic', 'regular')
@@ -325,17 +338,16 @@ def build():
                   'media/css/*.css']:
             sed_html = []
             sed_css = []
-            files = local("echo %s" % p, capture=True).split(" ")
+            files = c.run("echo %s" % p, hide=True).stdout.strip().split(" ")
             for f in files:
                 # Compute hash
-                md5 = local("md5sum %s" % f, capture=True).split(" ")[0][:14]
-                sha = local("openssl dgst -sha256 -binary %s | openssl enc -base64 -A" % f,
-                            capture=True)
-                print "[+] MD5/SHA hash for %s is %s and %s" % (f, md5, sha)
+                md5 = c.run("md5sum %s" % f, hide="out").stdout.split(" ")[0][:14]
+                sha = c.run("openssl dgst -sha256 -binary %s | openssl enc -base64 -A" % f,
+                            hide="out").stdout.strip()
                 # New name
                 root, ext = os.path.splitext(f)
                 newname = "%s.%s%s" % (root, md5, ext)
-                local("cp %s %s" % (f, newname))
+                c.run("cp %s %s" % (f, newname))
                 # Remove deploy/media
                 f = f[len('media/'):]
                 newname = newname[len('media/'):]
@@ -350,68 +362,67 @@ def build():
                          r"\1\2\3{}{}\3 \1integrity=\3sha256-{}\3 crossorigin=\3anonymous\3,"
                          r"g").format(media, f, media, newname, sha))
             if sed_css:
-                local("find . -name '*.css' -type f -print0 | "
+                c.run("find . -name '*.css' -type f -print0 | "
                       "xargs -r0 -n10 -P5 sed -i {}".format(
                           " ".join(("-e '{}'".format(x) for x in sed_css))))
             if sed_html:
-                local("find . -name '*.html' -type f -print0 | "
+                c.run("find . -name '*.html' -type f -print0 | "
                       "xargs -r0 -n10 -P5 sed -i {}".format(
                           " ".join(('-e "{}"'.format(x) for x in sed_html))))
 
-
         # Fix permissions
-        local(r"find * -type f -print0 | xargs -r0 chmod a+r")
-        local(r"find * -type d -print0 | xargs -r0 chmod a+rx")
+        c.run(r"find * -type f -print0 | xargs -r0 chmod a+r")
+        c.run(r"find * -type d -print0 | xargs -r0 chmod a+rx")
 
         # For videos and files, use symlinks
-        local("find media/files media/videos -type f -print0 "
+        c.run("find media/files media/videos -type f -print0 "
               "  | xargs -0 -I'{}' ln -sf $PWD/../content/'{}' '{}'")
 
-        local("git add *")
-        local("git diff --stat HEAD || true")
+        c.run("git add *")
+        c.run("git diff --stat HEAD || true")
         if confirm("More diff?", default=True):
-            local("git diff --word-diff HEAD || true")
+            c.run("git diff --word-diff HEAD || true", pty=True)
         if confirm("Keep?", default=True):
-            local('git commit -a -m "Autocommit"')
+            c.run('git commit -a -m "Autocommit"')
         else:
-            local("git reset --hard")
-            local("git clean -d -f")
-            abort("Build rollbacked")
+            c.run("git reset --hard")
+            c.run("git clean -d -f")
+            raise("Build rollbacked")
 
 @task
-def push(clean=False):
+def push(c, clean=False):
     """Push built site to production"""
-    local("git push github")
+    c.run("git push github")
 
-    with lcd(".final"):
+    with c.cd(".final"):
         # Restore timestamps (this relies on us not truncating
         # history too often)
-        local('''
+        c.run('''
 for f in $(git ls-tree -r -t --full-name --name-only HEAD); do
     touch -d $(git log --pretty=format:%cI -1 HEAD -- "$f") "$f";
 done''')
 
     # media
     for host in hosts:
-        local("rsync --exclude=.git --copy-unsafe-links -rt "
+        c.run("rsync --exclude=.git --copy-unsafe-links -rt "
               ".final/media/ {}:/data/webserver/media.luffy.cx/".format(host))
 
     # HTML
     for host in hosts:
-        local("rsync --exclude=.git --exclude=media "
+        c.run("rsync --exclude=.git --exclude=media "
               "--delete-delay --copy-unsafe-links -rt "
               ".final/ {}:/data/webserver/vincent.bernat.ch/".format(host))
 
     if clean:
         for host in hosts:
-            local("rsync --exclude=.git --copy-unsafe-links -rt "
+            c.run("rsync --exclude=.git --copy-unsafe-links -rt "
                   "--delete-delay --exclude=videos/\\*/ "
                   ".final/media/ {}:/data/webserver/media.luffy.cx/".format(host))
 
 @task
-def analytics():
+def analytics(c):
     """Get some stats"""
-    local("for h in {};"
+    c.run("for h in {};"
           "do ssh $h zcat -f /var/log/nginx/vincent.bernat.ch.log\\*"
           "   | grep -v atom.xml;"
           "done"
@@ -433,4 +444,4 @@ def analytics():
           "                            --sort-panel=REFERRING_SITES,BY_VISITORS,DESC "
           "                            --sort-panel=STATUS_CODES,BY_VISITORS,DESC "
           "".format(" ".join(hosts)))
-    local("x-www-browser goaccess.html")
+    c.run("x-www-browser goaccess.html")
