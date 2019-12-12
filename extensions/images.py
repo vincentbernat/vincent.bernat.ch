4c3d1faceed181ce72fd08300986c70574a25cf9
Vincent Bernat <vincent@bernat.ch>
2019-12-12 18:45:41 +0100

    images: don't add crossorigin to video
    This would mean a CORS request will be done for the poster. I don't
    remember why I have added that for the subtitles...

--- extensions/images.py
+++ extensions/images.py
@@ -301,7 +301,6 @@ class ImageFixerPlugin(Plugin):
                 img[0].tag = 'video'
                 img[0].set("controls", None)
                 img.attr("preload", "none")
-                img.attr("crossorigin", "anonymous")
                 img.attr("poster", self.site.media_url(
                     'images/posters/{}.jpg'.format(id)))
                 del img.attr.src
