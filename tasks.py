680d09708ffff31385b221a8d3a4514d42ca6d80
Vincent Bernat <vincent@bernat.ch>
2020-02-03 08:09:28 +0100

    tasks: don't accept to build if repository is dirty

--- tasks.py
+++ tasks.py
@@ -344,7 +344,7 @@ Info:    {infostring}""".format(**row))
 def build(c):
     """Build production content"""
     c.run("[ $(git rev-parse --abbrev-ref HEAD) = latest ]")
-    c.run("git annex lock")
+    c.run('git annex lock && [ -z "$(git status --porcelain)" ]')
     with c.cd("content/en"):
         c.run("! git grep -Pw '((?i:"
               "obviously|basically|simply|clearly|everyone knows|turns out"
