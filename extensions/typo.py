40d4b82d14f8ad504f880123f6ef68cf8298e433
Vincent Bernat <vincent@bernat.ch>
2020-06-06 23:41:48 +0200

    css: don't style ampersand
    This makes CSS smaller and Chrome seems to have a hard time with the
    webfont specified as a data URL.

--- extensions/typo.py
+++ extensions/typo.py
@@ -18,7 +18,7 @@ class TypographyPlugin(Plugin):
         # Patch typogrify
         from typogrify import filters
         original_process_ignores = filters.process_ignores
-        filters.applyfilters = lambda text: self.owntypo(filters.smartypants(filters.amp(text)))
+        filters.applyfilters = lambda text: self.owntypo(filters.smartypants(text))
         filters.process_ignores = self.process_ignores(original_process_ignores)
 
         # Don't use widont
