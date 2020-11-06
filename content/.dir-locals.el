da7744067abf95c4f266aca62c3470bfa2ef119f
Vincent Bernat <vincent@bernat.ch>
2020-11-06 19:55:54 +0100

    content: remove all file-local variables
    And use dir-local variables instead.

---
+++ content/.dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((indent-tabs-mode . t)))
+ ;; Use markdown-mode instead of HTML modes. This is a hack as Emacs
+ ;; documentation mentions minor modes, not major ones. The evaluation
+ ;; is not recursive.
+ (web-mode . ((mode . markdown)))
+ (html-mode . ((mode . markdown))))
