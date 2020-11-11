1fface56f98129860ec71f2cb9aead9006fd3d0d
Vincent Bernat <vincent@bernat.ch>
2020-11-11 13:38:04 +0100

    emacs: don't use tabs!

--- content/.dir-locals.el
+++ content/.dir-locals.el
@@ -1,4 +1,4 @@
-((nil . ((indent-tabs-mode . t)))
+((nil . ((indent-tabs-mode . nil)))
  ;; Use markdown-mode instead of HTML modes. This is a hack as Emacs
  ;; documentation mentions minor modes, not major ones. The evaluation
  ;; is not recursive.
