9c5af2902a0796539be85dbaf37955cd8a4293d6
Vincent Bernat <vincent@bernat.ch>
2021-05-26 19:49:07 +0200

    js: implement web share API

---
+++ content/media/js/luffy.share.js
@@ -0,0 +1,18 @@
+/* Handle Web Share API */
+
+luffy.do(() => {
+  const el = document.querySelector(".lf-webshare > a");
+  if (!el) return;
+  if (!navigator.share) {
+    el.parentNode.style.display = "none";
+    return;
+  }
+
+  el.addEventListener('click', event => {
+    event.preventDefault();
+    navigator.share({
+      text: document.title,     // title is usually ignored
+      url: document.querySelector('link[rel=canonical]').href
+    })
+  });
+});
