f9a74a49fcd5b961d03e17d3c15f721c32d5dad1
Vincent Bernat <vincent@bernat.ch>
2021-06-08 08:15:38 +0200

    Revert "build: keep font hints and subroutines"
    This reverts commit a0a3f3e31b98e5e28fd6de6e5a89b5c991f00270. Double
    the font size. Not good.

--- tasks.py
+++ tasks.py
@@ -449,6 +449,7 @@ printf " JPG %10s %10s %10s\n" \
         def subset(font, glyphs, options=[]):
             options = " ".join(["--layout-features=",
                                 "--text-file=../glyphs-{}.txt".format(glyphs),
+                                "--no-hinting --desubroutinize",
                                 *options])
             c.run("pyftsubset media/fonts/{}.woff "
                   "--flavor=woff --with-zopfli {}".format(font, options))
