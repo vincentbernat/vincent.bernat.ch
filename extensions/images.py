ce7922022e61ef91369482ced6abb15a532966e6
Vincent Bernat <vincent@bernat.ch>
2021-06-13 12:35:23 +0200

    Revert "images: use an iframe to display PDF"
    This reverts commit eef22938bbd41c2c23bcb660ad77c154946b9a45. It seems
    better to use our own download link than having Firefox and possibly
    others display a download prompt.

--- extensions/images.py
+++ extensions/images.py
@@ -336,14 +336,16 @@ class ImageFixerPlugin(Plugin):
 
             # PDF files
             elif src.endswith('.pdf'):
-                img[0].tag = 'iframe'
+                img[0].tag = 'object'
+                img.attr("type", "application/pdf")
                 # Most browsers: toolbar, navpanes, scrollbar
                 # Firefox: view, pagemode, zoom
-                img.attr("src", f"{src}#toolbar=0&navpanes=0&scrollbar=0&view=Fit&pagemode=none&zoom=page-fit")
-                img.attr("allowfullscreen", "true")
-                img.attr("loading", "lazy")
-                if img.attr.alt:
-                    img.attr("title", img.attr.alt)
+                img.attr("data", f"{src}#toolbar=0&navpanes=0&scrollbar=0&view=Fit&pagemode=none&zoom=page-fit")
+                fallback = pq('<a />')
+                fallback.attr("href", src)
+                fallback.text(img.attr.alt or "PDF")
+                img.append(fallback)
+                del img.attr.src
                 del img.attr.alt
 
             # On-demand videos (should be in /videos)
