80c7b29305dfab4894720fbb48e5f147afcb72f0
Vincent Bernat <vincent@bernat.ch>
2022-05-05 09:22:37 +0200

    build: switch to auto-mode-alist in .dir-locals

--- content/.dir-locals.el
+++ content/.dir-locals.el
@@ -1,6 +1,2 @@
 ((nil . ((indent-tabs-mode . nil)))
- ;; Use markdown-mode instead of HTML modes. This is a hack as Emacs
- ;; documentation mentions minor modes, not major ones. The evaluation
- ;; is not recursive.
- (web-mode . ((mode . markdown)))
- (html-mode . ((mode . markdown))))
+ (auto-mode-alist . (("\\.html\\'" . markdown-mode))))
