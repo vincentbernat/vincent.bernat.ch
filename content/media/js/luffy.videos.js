3e9c6a8129c763a6e855a42671ac021f63f47329
Vincent Bernat <vincent@bernat.ch>
2022-11-04 22:21:21 +0100

    js: don't try to load videos.js if we don't have a m3u8 video

--- content/media/js/luffy.videos.js
+++ content/media/js/luffy.videos.js
@@ -1,4 +1,4 @@
 luffy.do(() => {
-    if (!document.querySelector("video")) return;
+    if (!document.querySelector("video.lf-media source[type='application/vnd.apple.mpegurl']")) return;
     luffy.load("luffy1.videos.js");
 });
