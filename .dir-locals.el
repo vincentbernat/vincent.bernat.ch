cd58b4b5348b29934aa4d7f85d2da0717f9f6496
Vincent Bernat <vincent@bernat.ch>
2022-12-11 10:47:04 +0100

    extensions: use short links for RFCs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((indent-tabs-mode . nil)))
  (nix-mode . ((mode . apheleia)))
+ (python-mode . ((mode . apheleia)))
  (auto-mode-alist . (("\\.html\\'" . markdown-mode))))
---
+++ extensions/markdown/shortlinks.py
@@ -0,0 +1,27 @@
+"""Add additional schemas for URL."""
+
+import re
+
+from markdown.extensions import Extension
+from markdown.preprocessors import Preprocessor
+
+
+class ShortLinksExtension(Extension):
+    def extendMarkdown(self, md, md_globals):
+        md.registerExtension(self)
+        md.preprocessors.add("shortlinks", ShortLinksPreprocessor(md), "<reference")
+
+
+class ShortLinksPreprocessor(Preprocessor):
+    SCHEMAS = {"rfc": "https://www.rfc-editor.org/rfc/{}"}
+    RE = re.compile(rf"({'|'.join(SCHEMAS.keys())})://(\w+)", re.ASCII)
+
+    def run(self, lines):
+        return [
+            self.RE.sub(lambda mo: self.SCHEMAS[mo.group(1)].format(mo.group(2)), l)
+            for l in lines
+        ]
+
+
+def makeExtension(*args, **kwargs):
+    return ShortLinksExtension(*args, **kwargs)
