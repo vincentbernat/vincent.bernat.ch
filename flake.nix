d6c0e91f1ec0761ad2b87a745b9f3c72f486f24e
Vincent Bernat <vincent@bernat.ch>
2023-11-14 21:06:47 +0100

    js: update svgo
    Remove hack about cleanup ids

--- flake.nix
+++ flake.nix
@@ -108,7 +108,6 @@
                       name: 'preset-default',
                       params: {
                         overrides: {
-                          cleanupIds: false,
                           removeEmptyContainers: false,
                         }
                       }
