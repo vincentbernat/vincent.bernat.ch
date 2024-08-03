9b02ec896173415dbf0a872b37951fa231ed2c34
Vincent Bernat <vincent@bernat.ch>
2024-08-03 12:04:03 +0200

    Revert "build: try again AVIF for PNG images"
    This reverts commit 7ac4d5f4cb37e89d06b97fcd03565a90abab907b. Still no
    way to have a better compression with AVIF.

--- flake.nix
+++ flake.nix
@@ -168,10 +168,6 @@
                 # PNG→WebP
                 find $out -type f -name '*.png' -print0 \
                     | xargs -0 -P$(nproc) -i ${libwebp}/bin/cwebp -z 8 '{}' -o '{}'.webp
-
-                # PNG→AVIF
-                find $out -type f -name '*.png' -print0 \
-                    | xargs -0 -P$(nproc) -i ${libavif}/bin/avifenc -s 0 --lossless '{}' '{}'.avif
               '';
               installPhase = "true";
             };
