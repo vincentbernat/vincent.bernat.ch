667de05b0cd44289b2b1740296a7fa575a51dd91
Vincent Bernat <vincent@bernat.ch>
2026-04-01 08:11:07 +0200

    js: add a shortcut to display grid

--- content/media/js/luffy.shortcut.js
+++ content/media/js/luffy.shortcut.js
@@ -3,9 +3,13 @@
 luffy.do(() => {
   const el = document.querySelector("#lf-search-query");
   document.addEventListener("keydown", (event) => {
-    if (event.ctrlKey && event.key === "k") {
+    if (event.ctrlKey && !event.altKey && !event.shiftKey && !event.metaKey && event.key === "k") {
+      event.preventDefault();
       el.focus();
-      return true;
+    }
+    if (event.ctrlKey && event.altKey && !event.shiftKey && !event.metaKey && event.key === "g") {
+      document.body.classList.toggle("lf-debug-grid");
+      event.preventDefault();
     }
   });
 });
