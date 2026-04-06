6969b097eeba716c2d15449fac58d9fc558c4325
Vincent Bernat <vincent@bernat.ch>
2026-04-06 23:30:44 +0200

    js: use ctrl-k to focus search field on search page

--- content/media/js/luffy.shortcut.js
+++ content/media/js/luffy.shortcut.js
@@ -1,11 +1,11 @@
 /* Keyboard shortcuts */
 
 luffy.do(() => {
-  const el = document.querySelector("#lf-search-query");
+  const searchForm = document.querySelector("#lf-search-query") || document.querySelector("#lf-search-input");
   document.addEventListener("keydown", (event) => {
     if (event.ctrlKey && !event.altKey && !event.shiftKey && !event.metaKey && event.key === "k") {
       event.preventDefault();
-      el.focus();
+      searchForm.focus();
     }
     if (event.ctrlKey && event.altKey && !event.shiftKey && !event.metaKey && event.key === "g") {
       document.body.classList.toggle("lf-debug-grid");
