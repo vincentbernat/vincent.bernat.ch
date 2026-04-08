b8c2942c38b057a25e9ad61981454b6b6959aac4
Vincent Bernat <vincent@bernat.ch>
2026-04-08 05:09:57 +0200

    js: don't focus search form if already focused

--- content/media/js/luffy.shortcut.js
+++ content/media/js/luffy.shortcut.js
@@ -4,8 +4,10 @@ luffy.do(() => {
   const searchForm = document.querySelector("#lf-search-query") || document.querySelector("#lf-search-input");
   document.addEventListener("keydown", (event) => {
     if (event.ctrlKey && !event.altKey && !event.shiftKey && !event.metaKey && event.key === "k") {
-      event.preventDefault();
-      searchForm.focus();
+      if (document.activeElement !== searchForm) {
+        event.preventDefault();
+        searchForm.focus();
+      }
     }
     if (event.ctrlKey && event.altKey && !event.shiftKey && !event.metaKey && event.key === "g") {
       document.body.classList.toggle("lf-debug-grid");
