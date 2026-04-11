49deece30aca8f7c344dbc3304dbe5954a90fb62
Vincent Bernat <vincent@bernat.ch>
2026-04-11 07:48:47 +0200

    js: don't hardcode search URL into JS
    Put it in the dataset of the form.

--- content/media/js/luffy.search.js
+++ content/media/js/luffy.search.js
@@ -1,9 +1,6 @@
 // Override navbar search to use local Pagefind search
 luffy.do(() => {
   const form = document.getElementById("lf-search");
-  if (!form) return;
-  const lang = document.documentElement.lang;
-  const devMode = location.pathname.endsWith(".html");
-  form.action = `/${lang}/search${devMode ? ".html" : ""}`;
-  form.querySelectorAll("input[type=hidden]").forEach((el) => el.remove());
+  form?.setAttribute("action", form.dataset.action);
+  form?.querySelectorAll("input[type=hidden]").forEach((el) => el.remove());
 });
