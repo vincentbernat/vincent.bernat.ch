c9b6c7b39bd16b5aea5644276c41876a27c2f8ee
Vincent Bernat <vincent@bernat.ch>
2026-05-14 20:17:26 +0200

    extensions: move toc.FixTOC plugin to text.TOCPlugin

--- extensions/text.py
+++ extensions/text.py
@@ -128,3 +128,17 @@ process.stdin.pipe(split('\\0', null, { trailing: false })).on('data', function(
         if resource.source_file.kind != "html":
             return
         return self.RE.sub(self.katex_render, text)
+
+
+class TOCPlugin(Plugin):
+    """Remove whitespaces inside UL"""
+
+    def text_resource_complete(self, resource, text):
+        if not resource.source_file.kind == "html":
+            return
+        d = pq(text, parser="html")
+        for toc in d.items(".toc"):
+            html = toc.outer_html()
+            html = re.sub(r"</li>\s+</ul>", "</li></ul>", html)
+            toc.replace_with(html)
+        return "<!DOCTYPE html>\n" + d.outer_html()
