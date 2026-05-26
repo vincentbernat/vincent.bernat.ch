9f8649ae2f1012a59231adb6fe7d6429cda906a5
Vincent Bernat <vincent@bernat.ch>
2026-05-26 22:38:00 +0200

    Revert "js: wait for load event before sending a hit"
    This reverts commit 375dcbc81008211d34db60b0e5db9563e6998f40.

--- content/media/js/luffy.analytics.js
+++ content/media/js/luffy.analytics.js
@@ -1,16 +1,14 @@
 /* Simple analytics. Half-stolen from Bear Blog. */
 
 luffy.do(() => {
-    document.addEventListener("load", () => {
-        if (navigator.webdriver) return;
-        const sendHit = () => {
-            new Image().src = `/hit${window.location.pathname}`;
-        };
-        document.addEventListener("touchmove", sendHit, {
-            once: true,
-        });
-        document.addEventListener("mousemove", sendHit, {
-            once: true,
-        });
+    if (navigator.webdriver) return;
+    const sendHit = () => {
+        new Image().src = `/hit${window.location.pathname}`;
+    };
+    document.addEventListener("touchmove", sendHit, {
+        once: true,
+    });
+    document.addEventListener("mousemove", sendHit, {
+        once: true,
     });
 });
