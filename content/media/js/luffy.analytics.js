375dcbc81008211d34db60b0e5db9563e6998f40
Vincent Bernat <vincent@bernat.ch>
2026-05-26 22:22:10 +0200

    js: wait for load event before sending a hit

--- content/media/js/luffy.analytics.js
+++ content/media/js/luffy.analytics.js
@@ -1,14 +1,16 @@
 /* Simple analytics. Half-stolen from Bear Blog. */
 
 luffy.do(() => {
-    if (navigator.webdriver) return;
-    const sendHit = () => {
-        new Image().src = `/hit${window.location.pathname}`;
-    };
-    document.addEventListener("touchmove", sendHit, {
-        once: true,
-    });
-    document.addEventListener("mousemove", sendHit, {
-        once: true,
+    document.addEventListener("load", () => {
+        if (navigator.webdriver) return;
+        const sendHit = () => {
+            new Image().src = `/hit${window.location.pathname}`;
+        };
+        document.addEventListener("touchmove", sendHit, {
+            once: true,
+        });
+        document.addEventListener("mousemove", sendHit, {
+            once: true,
+        });
     });
 });
