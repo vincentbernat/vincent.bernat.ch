60241044e149281ea8f204cfbeb98671078d2819
Vincent Bernat <vincent@bernat.ch>
2026-05-27 19:03:28 +0200

    js: use canonical URL
    Otherwise, we may have an infinite number of paths by just adding some
    slashes.

--- content/media/js/luffy.analytics.js
+++ content/media/js/luffy.analytics.js
@@ -1,9 +1,11 @@
 /* Simple analytics. Half-stolen from Bear Blog. */
 
 luffy.do(() => {
-    if (navigator.webdriver) return;
+    const canonical = document.querySelector('link[rel="canonical"]')?.href;
+    if (navigator.webdriver || !canonical) return;
+    const url = new URL(canonical);
     const sendHit = () => {
-        new Image().src = `/hit${window.location.pathname}`;
+        new Image().src = `/hit/${url.hostname}${url.pathname}`;
     };
     document.addEventListener("touchmove", sendHit, {
         once: true,
