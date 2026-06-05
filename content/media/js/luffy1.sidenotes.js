23ea7ba6e527f9299ba66416b69cc69599c0cb17
Vincent Bernat <vincent@bernat.ch>
2026-06-05 15:13:09 +0200

    js: add a check for overlapping sidenotes

---
+++ content/media/js/luffy1.sidenotes.js
@@ -0,0 +1,29 @@
+/* Some browsers support CSS anchor positioning but compute anchored sidenote
+   positions incorrectly, leaving them stacked on top of each other. Detect
+   such overlaps and fall back to inline sidenotes when they happen. */
+
+luffy.do(() => {
+  const main = document.querySelector(".lf-main");
+  const notes = [...main.querySelectorAll(".lf-sidenote")];
+  if (!main || !CSS.supports("anchor-name: --a") || notes.length < 2) return;
+
+  const check = () => {
+    /* Wait for sidenotes to be in the margin. Then check if they are
+       overlapping and toggle lf-sidenotes-overlap class in this case. */
+    if (getComputedStyle(notes[0]).position !== "absolute") return;
+    ro.disconnect();
+    const rects = notes
+      .map((n) => n.getBoundingClientRect())
+      .sort((a, b) => a.top - b.top);
+    for (let i = 1; i < rects.length; i++) {
+      if (rects[i].top < rects[i - 1].bottom) {
+        console.info("Apply workaround for overlapping sidenotes");
+        main.classList.add("lf-sidenotes-overlap");
+        return;
+      }
+    }
+  };
+
+  const ro = new ResizeObserver(check);
+  ro.observe(main);
+});
