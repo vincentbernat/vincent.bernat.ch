0fdc3a70da5ab99cd3b66fedd0f26b028444e41c
Vincent Bernat <vincent@bernat.ch>
2026-06-20 21:38:25 +0200

    js: only use data-integrity/href/src when loading scripts
    At some point, we could also accept data-module.

--- content/media/js/luffy.js
+++ content/media/js/luffy.js
@@ -5,7 +5,6 @@ combine:
     where: bottom
     remove: yes
 ---
-
 window.luffy = {
   do(fn) {
     // Create a scope, catch errors to not propagate further, allow early return.
@@ -16,7 +15,9 @@ window.luffy = {
     }
   },
   load(what, onload) {
-    // Lazy loading of some resources.
+    // Lazy loading of some resources. `data-src`, `data-href`, `data-integrity`
+    // attributes are copied to `src`, `href`, `integrity` respectively.
+    //
     //  <script data-src="..." data-name="gallery.js"></script>
     //  <link rel="stylesheet" data-href="..." href="data:text/css;base64," data-name="gallery.css">
     const el = document.querySelector(
@@ -24,8 +25,7 @@ window.luffy = {
     );
     if (!el) throw `cannot load ${what}`;
     if (onload) el.onload = onload;
-    for (const attr in el.dataset) {
-      if (attr != "name") el[attr] = el.dataset[attr];
-    }
+    for (const k of ["integrity", "href", "src"])
+      if (el.dataset[k]) el[k] = el.dataset[k];
   },
 };
