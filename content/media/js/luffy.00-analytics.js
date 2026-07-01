1658cefe8a0c615a6158df940a76cd88657af0cc
Vincent Bernat <vincent@bernat.ch>
2026-07-01 22:22:54 +0200

    js: round width down a bit more if the screen is large

--- content/media/js/luffy.00-analytics.js
+++ content/media/js/luffy.00-analytics.js
@@ -20,7 +20,12 @@ luffy.do(() => {
             q: vars.query ?? location.search,
             s:
                 vars.screen ??
-                `${Math.floor(document.documentElement.clientWidth / 10) * 10}`,
+                ((w) => {
+                    // Round down to 10px (when < 1000), 20px (when < 2000),
+                    // 40px (when < 3000).
+                    const step = 10 * 2 ** Math.min(2, Math.floor(w / 1000));
+                    return `${Math.floor(w / step) * step}`;
+                })(document.documentElement.clientWidth),
             e: !!vars.event,
             rnd: Math.random().toString(36).substr(2, 5),
         });
