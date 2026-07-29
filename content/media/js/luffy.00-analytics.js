22f95e05f4df59d782dc6c3dead4f9ebf9296c65
Vincent Bernat <vincent@bernat.ch>
2026-07-29 21:31:25 +0200

    js: prettier analytics JS file
    Dunno why the indent was not like the other files.

--- content/media/js/luffy.00-analytics.js
+++ content/media/js/luffy.00-analytics.js
@@ -2,49 +2,49 @@
    filter bots was stolen from Bear Blog. */
 
 luffy.do(() => {
-    const canonical = document.querySelector('link[rel="canonical"]')?.href;
-    if (
-        !canonical ||
-        navigator.webdriver ||
-        (localStorage && localStorage.getItem("skipgc"))
-    )
-        return;
-    const url = new URL(canonical);
+  const canonical = document.querySelector('link[rel="canonical"]')?.href;
+  if (
+    !canonical ||
+    navigator.webdriver ||
+    (localStorage && localStorage.getItem("skipgc"))
+  )
+    return;
+  const url = new URL(canonical);
 
-    /* Send a hit to GoatCounter through the /hit endpoint. It may not exist, use `luffy.count?.()`. */
-    luffy.count = (vars = {}) => {
-        const params = new URLSearchParams({
-            p: vars.event ?? vars.path ?? url.pathname,
-            t: vars.title ?? document.title,
-            r: vars.referrer ?? document.referrer,
-            q: vars.query ?? location.search,
-            s:
-                vars.screen ??
-                ((w) => {
-                    // Round down to 10px (when < 1000), 20px (when < 2000),
-                    // 40px (when < 3000).
-                    const step = 10 * 2 ** Math.min(2, Math.floor(w / 1000));
-                    return `${Math.floor(w / step) * step}`;
-                })(document.documentElement.clientWidth),
-            e: !!vars.event,
-            rnd: Math.random().toString(36).substr(2, 5),
-        });
-        fetch(`/hit?${params}`, { keepalive: true }).catch(() => {});
-    };
+  /* Send a hit to GoatCounter through the /hit endpoint. It may not exist, use `luffy.count?.()`. */
+  luffy.count = (vars = {}) => {
+    const params = new URLSearchParams({
+      p: vars.event ?? vars.path ?? url.pathname,
+      t: vars.title ?? document.title,
+      r: vars.referrer ?? document.referrer,
+      q: vars.query ?? location.search,
+      s:
+        vars.screen ??
+        ((w) => {
+          // Round down to 10px (when < 1000), 20px (when < 2000),
+          // 40px (when < 3000).
+          const step = 10 * 2 ** Math.min(2, Math.floor(w / 1000));
+          return `${Math.floor(w / step) * step}`;
+        })(document.documentElement.clientWidth),
+      e: !!vars.event,
+      rnd: Math.random().toString(36).substr(2, 5),
+    });
+    fetch(`/hit?${params}`, { keepalive: true }).catch(() => {});
+  };
 
-    let sent = false;
-    const sendHit = () => {
-        if (sent) return;
-        sent = true;
-        luffy.count();
-    };
+  let sent = false;
+  const sendHit = () => {
+    if (sent) return;
+    sent = true;
+    luffy.count();
+  };
 
-    /* Assume we are human if we trigger one of these interactions. pointerdown
+  /* Assume we are human if we trigger one of these interactions. pointerdown
        happen a bit before click and gives more time to send a beacon. */
-    ["touchmove", "mousemove", "keydown", "pointerdown"].forEach((eventName) =>
-        document.addEventListener(eventName, sendHit, {
-            once: true,
-            passive: true,
-        }),
-    );
+  ["touchmove", "mousemove", "keydown", "pointerdown"].forEach((eventName) =>
+    document.addEventListener(eventName, sendHit, {
+      once: true,
+      passive: true,
+    }),
+  );
 });
