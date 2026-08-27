ba1efb99b999cdeb41e8e293ab06cbdc32c32219
Vincent Bernat <vincent@bernat.ch>
2026-08-27 10:45:24 +0200

    build: do not patch nginx
    This is silly for just `/`.

--- flake.nix
+++ flake.nix
@@ -135,27 +135,6 @@
         fonttools = pkgs.python3Packages.fonttools.overridePythonAttrs (old: {
           dependencies = (old.dependencies or [ ]) ++ old.optional-dependencies.woff;
         });
-        nginx = (pkgs.nginxStable.override {
-          modules =
-            let
-              acceptlanguage = {
-                name = "accept-language";
-                src = pkgs.fetchFromGitHub {
-                  name = "accept-language";
-                  owner = "giom";
-                  repo = "nginx_accept_language_module";
-                  rev = "2f69842f83dac77f7d98b41a2b31b13b87aeaba7";
-                  hash = "sha256-fMENKki03aQmw2rX8gMmdwnGUBL4qsPHEAXOEmjWXsI=";
-                };
-                meta = {
-                  description = "Parse Accept-Language header";
-                  license = with l.licenses; [ bsd2 ];
-                };
-              };
-            in
-            [ acceptlanguage ];
-        });
-
       in
       {
         apps = {
