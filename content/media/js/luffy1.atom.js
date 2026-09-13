64bf5fa91076033d3780a837fb0f76d0640a8cf4
Vincent Bernat <vincent@bernat.ch>
2026-09-13 07:17:11 +0200

    content: redirect Atom feed to HTML version with meta refresh
    Still keep the JS to masquerade the URL.

--- content/media/js/luffy1.atom.js
+++ content/media/js/luffy1.atom.js
@@ -1,8 +1,5 @@
-/* Inside the Atom feed, load the HTML rendering. In the HTML version, display
-   the Atom feed URL. */
-if (document instanceof XMLDocument) {
-  location.replace(document.querySelector("noscript a").href);
-} else {
-  const feed = document.querySelector('link[type="application/atom+xml"]');
-  if (feed) history.replaceState(null, "", feed.href);
-}
+/* In the HTML rendering of the Atom feed, display the Atom feed URL. */
+luffy.do(() => {
+    const feed = document.querySelector('link[type="application/atom+xml"]');
+    if (feed) history.replaceState(null, "", feed.href);
+});
