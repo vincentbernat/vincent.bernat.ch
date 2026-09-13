21905f1127b7fd2878edda51840d2a4ca55adb46
Vincent Bernat <vincent@bernat.ch>
2026-09-13 07:12:24 +0200

    content: simplify JS to display HTML version of the Atom feed
    Redirect to it with JS and replace the displayed URL.

--- content/media/js/luffy1.atom.js
+++ content/media/js/luffy1.atom.js
@@ -1,26 +1,8 @@
-/* Replace the Atom feed with its web page when viewed in a browser. The link to
-   this page is in the noscript element. */
-(() => {
-  const link = document.querySelector("noscript a");
-  if (!link || !window.fetch || !window.DOMParser) return showFeed();
-  const xhtml = "http://www.w3.org/1999/xhtml";
-  fetch(link.href)
-    .then((response) => response.text())
-    .then((html) => {
-      const page = new DOMParser().parseFromString(html, "text/html");
-      document.replaceChild(
-        document.adoptNode(page.documentElement),
-        document.documentElement,
-      );
-      document.documentElement.classList.add("rss");
-
-      // Scripts moved from another document do not run. Create new ones.
-      for (const old of document.querySelectorAll("script")) {
-        const script = document.createElementNS(xhtml, "script");
-        for (const { name, value } of old.attributes)
-          script.setAttribute(name, value);
-        script.textContent = old.textContent;
-        old.replaceWith(script);
-      }
-    });
-})();
+/* Inside the Atom feed, load the HTML rendering. In the HTML version, display
+   the Atom feed URL. */
+if (document instanceof XMLDocument) {
+  location.replace(document.querySelector("noscript a").href);
+} else {
+  const feed = document.querySelector('link[type="application/atom+xml"]');
+  if (feed) history.replaceState(null, "", feed.href);
+}
