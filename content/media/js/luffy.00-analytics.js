7caec301ab340d5eb520cce0178becc745cd4e69
Vincent Bernat <vincent@bernat.ch>
2026-09-15 06:28:31 +0200

    js: simplify analytics script a bit more
    Only accept event and title as parameters as everything else is not
    used.

--- content/media/js/luffy.00-analytics.js
+++ content/media/js/luffy.00-analytics.js
@@ -2,31 +2,29 @@
    filter bots was stolen from Bear Blog. */
 
 luffy.do(() => {
-  const canonical = document.querySelector('link[rel="canonical"]')?.href;
+  const canonical = document.querySelector("link[rel=canonical]")?.href;
   if (
     !canonical ||
     navigator.webdriver ||
     (localStorage && localStorage.getItem("skipgc"))
   )
     return;
-  const url = new URL(canonical);
+  const path = new URL(canonical).pathname;
 
-  /* Send a hit to GoatCounter through the /hit endpoint. It may not exist, use `luffy.count?.()`. */
-  luffy.count = (vars = {}) => {
+  /* Send a hit to GoatCounter through the /hit endpoint. Call with `luffy.count?.()`. */
+  luffy.count = ({ event, title } = {}) => {
+    // Round down client width to 10px (when < 1000), 20px (when < 2000), 40px
+    // (otherwise).
+    const w = document.documentElement.clientWidth;
+    const step = 10 << Math.min(2, (w / 1000) | 0);
+
     const params = new URLSearchParams({
-      p: vars.event ?? vars.path ?? url.pathname,
-      t: vars.title ?? document.title,
-      r: vars.referrer ?? (vars.event ? canonical : document.referrer),
-      q: vars.query ?? location.search,
-      s:
-        vars.screen ??
-        ((w) => {
-          // Round down to 10px (when < 1000), 20px (when < 2000),
-          // 40px (when < 3000).
-          const step = 10 * 2 ** Math.min(2, Math.floor(w / 1000));
-          return `${Math.floor(w / step) * step}`;
-        })(document.documentElement.clientWidth),
-      e: !!vars.event,
+      p: event || path,
+      t: title || document.title,
+      r: event ? canonical : document.referrer,
+      q: location.search,
+      s: w - (w % step),
+      e: !!event,
       rnd: Math.random().toString(36).slice(2, 7),
     });
     fetch(`/hit?${params}`, { keepalive: true }).catch(() => {});
